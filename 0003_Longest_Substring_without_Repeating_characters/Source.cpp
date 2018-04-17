1fd9115a8d89fedde6d2295560b5a31c776f30b0
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-04-17 17:27:46 +0800

    0417_2018

--- 0003_Longest_Substring_without_Repeating_characters/Source.cpp
+++ 0003_Longest_Substring_without_Repeating_characters/Source.cpp
@@ -1,42 +1,34 @@
 
 #include<algorithm>
 #include<string>
-#include<map>
-#include<set>
+#include<array>
 
 using std::string;
-using std::map;
 
 
 
 class Solution {
 public:
 	int lengthOfLongestSubstring(const string s) {
-		map<char, int> char_index_map;
-
 		int max_len = 0;
-		int start = 0;
-
+		int start = -1;
 		for (int i = 0; i < s.size(); ++i) {
-			if (char_index_map.count(s[i] > 0)) {
-				max_len = std::max(max_len, i - start);
-				char_index_map.at(s[i]) = i;
-				start = i;
+			if (dict[s[i]] > start) {
+				start = dict[s[i]];
 			}
-			else {
-				char_index_map.insert({ s[i], i });
-				max_len = std::max(max_len, i - start);
-			}
-
+			dict[s[i]] = i;
+			max_len = std::max(max_len, i - start);
 		}
 		return max_len;
 	}
+private:
+	std::array<int, 255> dict{ 0 };
 };
 
 int main(void)
 {
 	Solution sln;
-	auto rtn = sln.lengthOfLongestSubstring("pwwkew");
+	auto rtn = sln.lengthOfLongestSubstring("bbbbbb");
 
 	return 0;
 }
---
+++ 0005_Longest_Palindromic_Substring/Source.cpp
@@ -0,0 +1,18 @@
+
+#include<string>
+
+using std::string;
+
+
+class Solution {
+public:
+	string longestPalindrome(string s) {
+
+	}
+};
+
+int main(void)
+{
+
+	return 0;
+}
