4758c4f687d2dc1c76bf2663253911f9c4efdf9e
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-04 15:07:53 +0800

    finish problem 5.

--- 0005_Longest_Palindromic_Substring/0005.cc
+++ 0005_Longest_Palindromic_Substring/0005.cc
@@ -1,18 +1,59 @@
 
-#include<string>
+#include <string>
 
 using std::string;
 
-
 class Solution {
-public:
-	string longestPalindrome(string s) {
-
-	}
+ public:
+  string longestPalindrome(string s) {
+    int max_length = 0;
+    int start_pos = 0;
+    for (int /*can't use unsigned type*/ i = 0; i < s.size(); ++i) {
+      int left_cursor = i, right_cursor = i;
+      int palindrome_length = 0;
+      bool found_palindrome = false;
+      while (left_cursor >= 0 && right_cursor < s.size()) {
+        if (s[left_cursor] == s[right_cursor])
+          palindrome_length = right_cursor - left_cursor + 1;
+        else {
+          --left_cursor;
+          ++right_cursor;
+          break;
+        }
+        --left_cursor;
+        ++right_cursor;
+      }
+      if (palindrome_length > max_length) {
+        max_length = palindrome_length;
+        start_pos = left_cursor + 1;
+      }
+      left_cursor = i;
+      right_cursor = i + 1;
+      found_palindrome = false;
+      while (left_cursor >= 0 && right_cursor < s.size()) {
+        if (s[left_cursor] == s[right_cursor])
+          palindrome_length = right_cursor - left_cursor + 1;
+        else {
+          --left_cursor;
+          ++right_cursor;
+          break;
+        }
+        --left_cursor;
+        ++right_cursor;
+      }
+      if (palindrome_length > max_length) {
+        max_length = palindrome_length;
+        start_pos = left_cursor + 1;
+      }
+    }
+    return string(s.begin() + start_pos, s.begin() + start_pos + max_length);
+  }
 };
 
-int main(void)
-{
-
-	return 0;
+int main(void) {
+  Solution sln;
+  auto rtn = sln.longestPalindrome("abaqwerabcfcba");
+	rtn = sln.longestPalindrome("abaqwerabba");
+	rtn = sln.longestPalindrome("abaqweraa");
+  return 0;
 }
