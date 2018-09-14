84feb5e6a3ee8f668dfa59af2eeb2add4656c82c
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-14 14:47:57 +0800

    0024 not finished

---
+++ 0024_Swap_Nodes_in_Pairs/0024.cc
@@ -0,0 +1,16 @@
+
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+ public:
+  ListNode* swapPairs(ListNode* head) {
+
+
+    return head;
+  }
+};
