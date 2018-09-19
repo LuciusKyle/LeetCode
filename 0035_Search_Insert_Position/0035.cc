a88ccea3247eca73855803f7ca5b509967c510a0
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-19 09:56:29 +0800

    0035 solved.

---
+++ 0035_Search_Insert_Position/0035.cc
@@ -0,0 +1,33 @@
+
+#include <vector>
+
+using std::vector;
+
+int searchInsert(int* nums, int numsSize, int target) {
+  if (numsSize == 0 || target <= nums[0]) return 0;
+  if (nums[numsSize - 1] < target) return numsSize;
+  int lower_index = 0;
+  int upper_index = numsSize - 1;
+  while (upper_index - lower_index > 1) {
+    const int mid_index = (lower_index + upper_index) / 2;
+    if (target < nums[mid_index]) {
+      upper_index = mid_index;
+      continue;
+    }
+    if (nums[mid_index] < target) {
+      lower_index = mid_index;
+      continue;
+    }
+    return mid_index;
+  }
+  if (nums[lower_index] == target) return lower_index;
+  if (target <= nums[upper_index]) return upper_index;
+  return -1;
+}
+
+int main(void) {
+  int arr[] = {1, 3, 5, 6};
+  int rtn = searchInsert(arr, std::end(arr) - std::begin(arr), 4);
+
+  return 0;
+}
