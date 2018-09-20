48767e789f206eef7cda9498db89dbb9b3af929e
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-20 16:43:53 +0800

    0051 wrong answer 2.

--- 0051_N-Queens/0051.cc
+++ 0051_N-Queens/0051.cc
@@ -1,9 +1,12 @@
 
 #include<vector>
 #include<string>
+#include<iostream>
 
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
 
 class Solution {
 public:
@@ -26,17 +29,19 @@ private:
 	void FillBoard(vector<vector<string>> &rtn, vector<string> &board, const size_t n, vector<string> &solution_set) {
 		const size_t edge_size = board.size();
 		for (size_t i = 0; i < edge_size; ++i) {
+			bool whole_line_is_invalid = true;
 			for (size_t ii = 0; ii < edge_size; ++ii) {
 				if (board[i][ii] == '.' && (n != edge_size || solution_set[i][ii] != 'Q')) {
+					whole_line_is_invalid = false;
 					PutChessman(i, ii, board);
 					if (n == 1) {
 						rtn.push_back(board);
 						for (size_t j = 0; j < edge_size; ++j)
 							for (size_t jj = 0; jj < edge_size; ++jj)
-								if (rtn.back()[j][jj] != 'Q')
-									rtn.back()[j][jj] = '.';
-								else
+								if (rtn.back()[j][jj] == 'Q')
 									solution_set[j][jj] = 'Q';
+								else
+									rtn.back()[j][jj] = '.';
 					}
 					else {
 						FillBoard(rtn, board, n - 1, solution_set);
@@ -44,22 +49,24 @@ private:
 					RemoveChessman(i, ii, board);
 				}
 			}
+			if (whole_line_is_invalid)
+				return;
 		}
 	}
 
 	void PutChessman(const size_t row, const size_t column, vector<string> &board) const {
 		const size_t edge_size = board.size();
 		for (size_t i = 0; i < edge_size; ++i) {
-			if (0 < row - i && 0 < column - i) {
+			if (i < row && i < column) {
 				++board[row - i][column - i];
 			}
 			if (row + i < edge_size && column + i < edge_size) {
 				++board[row + i][column + i];
 			}
-			if (row + i < edge_size && 0 < column - i) {
+			if (row + i < edge_size && i < column) {
 				++board[row + i][column - i];
 			}
-			if (0 < row - i && column + i < edge_size) {
+			if (i < row && column + i < edge_size) {
 				++board[row - i][column + i];
 			}
 			++board[row][i];
@@ -71,28 +78,37 @@ private:
 	void RemoveChessman(const size_t row, const size_t column, vector<string> &board) {
 		const size_t edge_size = board.size();
 		for (size_t i = 0; i < edge_size; ++i) {
-			if (0 < row - i && 0 < column - i && '.' < board[row - i][column - i]) {
-				++board[row - i][column - i];
+			if (i < row && i < column && '.' < board[row - i][column - i]) {
+				--board[row - i][column - i];
 			}
 			if (row + i < edge_size && column + i < edge_size && '.' < board[row + i][column + i]) {
-				++board[row + i][column + i];
+				--board[row + i][column + i];
 			}
-			if (row + i < edge_size && 0 < column - i && '.' < board[row + i][column - i]) {
-				++board[row + i][column - i];
+			if (row + i < edge_size && i < column && '.' < board[row + i][column - i]) {
+				--board[row + i][column - i];
 			}
-			if (0 < row - i && column + i < edge_size && '.' < board[row - i][column + i]) {
-				++board[row - i][column + i];
+			if (i < row && column + i < edge_size && '.' < board[row - i][column + i]) {
+				--board[row - i][column + i];
 			}
 			if ('.' < board[row][i])
-				++board[row][i];
+				--board[row][i];
 			if ('.' < board[i][column])
-				++board[i][column];
+				--board[i][column];
+			board[row][column] = '.';
 		}
 	}
 };
 
 int main(void) {
 	Solution sln;
-	auto rtn = sln.solveNQueens(4);
+	const auto rtn = sln.solveNQueens(4);
+	for (const auto &board : rtn) {
+		for (const auto &row : board) {
+			for (const auto ch : row)
+				cout << ch << '\t';
+			cout << '\n';
+		}
+		cout << "\n\n";
+	}
 	return 0;
 }
