f8081a03af21f82791c69854555315568678862f
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-25 14:07:38 +0800

    0058 solved.

--- 0058_Length_of_Last_Word/0058.cc
+++ 0058_Length_of_Last_Word/0058.cc
@@ -1,11 +1,36 @@
 
-#include<string>
+#include <assert.h>
+#include <string>
 
 using std::string;
 
 class Solution {
-public:
-    int lengthOfLastWord(string s) {
-        
-    }
+ public:
+  int lengthOfLastWord(const string s) const {
+    int rtn = 0;
+    bool start_count = false;
+    for (auto r_iter = s.crbegin(); r_iter != s.crend(); ++r_iter)
+      if (start_count)
+        if (*r_iter != ' ')
+          ++rtn;
+        else
+          return rtn;
+      else if (*r_iter != ' ') {
+        ++rtn;
+        start_count = true;
+      }
+    return rtn;
+  }
 };
+
+int main(void) {
+  Solution sln;
+  assert(sln.lengthOfLastWord("Hello World") == 5);
+  assert(sln.lengthOfLastWord("World") == 5);
+  assert(sln.lengthOfLastWord("a") == 1);
+  assert(sln.lengthOfLastWord("") == 0);
+  assert(sln.lengthOfLastWord(" ") == 0);
+  assert(sln.lengthOfLastWord("abc   ") == 3);
+  assert(sln.lengthOfLastWord("asdf abc   ") == 3);
+  return 0;
+}
