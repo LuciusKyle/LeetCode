c408d4737a7de429f98a444f56ae272b71c529eb
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-26 17:01:14 +0800

    0068 not finished.

---
+++ 0068_Text_Justification/0068.cc
@@ -0,0 +1,21 @@
+
+#include<string>
+#include<vector>
+#include<limits.h>
+
+using std::string;
+using std::vector;
+
+class Solution {
+ public:
+  vector<string> fullJustify(const vector<string>& words, int maxWidth) {
+    vector<size_t> costs(words.size(), INT_MAX);
+  }
+};
+
+int main(int argc, char const* argv[]) {
+  Solution sln;
+
+  return 0;
+}
+
