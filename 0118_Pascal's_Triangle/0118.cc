61e4c9f790bea9cc83f67ae1e1d3d2a832fb3d8c
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-09-29 09:26:14 +0800

    0118 and 0119 solved.

---
+++ 0118_Pascal's_Triangle/0118.cc
@@ -0,0 +1,40 @@
+
+#include<vector>
+
+using std::vector;
+
+
+// 0118 and 0119 share a same solution.
+class Solution {
+public:
+	vector<int> getRow(const int rowIndex) {
+		return generate(rowIndex + 1).back();
+	}
+	vector<vector<int>> generate(const int numRows) {
+		vector<vector<int>> rtn;
+		for (int i = 1; i <= numRows; ++i) {
+			rtn.push_back(vector<int>());
+			for (int ii = 0; ii < i; ++ii) {
+				if (ii == 0) {
+					rtn.back().push_back(1);
+					continue;
+				}
+				if (ii == i - 1) {
+					rtn.back().push_back(1);
+					continue;
+				}
+				rtn.back().push_back(rtn[i - 2][ii - 1] + rtn[i - 2][ii]);
+			}
+		}
+		return rtn;
+	}
+};
+
+
+int main(void)
+{
+	Solution sln;
+	auto rtn = sln.generate(5);
+	auto rtn1 = sln.getRow(0);
+	return 0;
+}
