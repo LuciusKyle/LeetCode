c2511c01ad0c386c558de39286e53ab3cba258e1
Lucius Kyle <LuciusKyle@outlook.com>
2018-10-11 15:41:09 +0000

    0084 15% beat.

--- 0084_Largest_Rectangle_in_Histogram/0084.cc
+++ 0084_Largest_Rectangle_in_Histogram/0084.cc
@@ -1,92 +1,36 @@
+#include <iostream>
 
 #include <limits.h>
 #include <algorithm>
 #include <map>
+#include <set>
 #include <vector>
 
 using std::multimap;
+using std::set;
 using std::vector;
-
-// 96 / 96 test cases passed. Status: Accepted. Runtime: 1168 ms.    5% beat.
-class Solution00 {
-public:
-	int largestRectangleArea(const vector<int>& heights) {
-		multimap<int, size_t> heights_index_map;
-		for(size_t i =0;i<heights.size();++i)
-			heights_index_map.insert({heights[i],i});
-		
-		return heights.empty() ? 0 : largestRectangleArea(heights, 0, heights.size() - 1);
-	}
-
-private:
-	int largestRectangleArea(const vector<int>& heights, const size_t lower_index, const size_t upper_index) {
-		if (upper_index - lower_index < 2)
-			return std::max({ heights[lower_index], heights[upper_index], std::min(heights[lower_index], heights[upper_index]) * static_cast<int>(upper_index - lower_index + 1) });
-
-		int min_val = INT_MAX;
-		size_t min_val_index = 0;
-		for (size_t i = lower_index; i <= upper_index; ++i)
-			if (heights[i] < min_val) {
-				min_val = heights[i];
-				min_val_index = i;
-			}
-		return std::max({ min_val * static_cast<int>(upper_index - lower_index + 1), min_val_index == lower_index? 0 : largestRectangleArea(heights, lower_index, min_val_index - 1), min_val_index == upper_index? 0 : largestRectangleArea(heights, min_val_index + 1, upper_index) });
-	}
-		int largestRectangleArea(const vector<int>& heights, const size_t lower_index, const size_t upper_index) {
-		if (upper_index - lower_index < 2)
-			return std::max({ heights[lower_index], heights[upper_index], std::min(heights[lower_index], heights[upper_index]) * static_cast<int>(upper_index - lower_index + 1) });
-
-		int min_val = INT_MAX;
-		size_t min_val_index = 0;
-		for (size_t i = lower_index; i <= upper_index; ++i)
-			if (heights[i] < min_val) {
-				min_val = heights[i];
-				min_val_index = i;
-			}
-		return std::max({ min_val * static_cast<int>(upper_index - lower_index + 1), min_val_index == lower_index? 0 : largestRectangleArea(heights, lower_index, min_val_index - 1), min_val_index == upper_index? 0 : largestRectangleArea(heights, min_val_index + 1, upper_index) });
-	}
-};
-
 
 class Solution {
 public:
 	int largestRectangleArea(const vector<int>& heights) {
-		return heights.empty() ? 0 : largestRectangleArea(heights, 0, heights.size() - 1);
-	}
-
-private:
-	int largestRectangleArea(const vector<int>& heights, const size_t lower_index, const size_t upper_index) {
-		if (upper_index - lower_index < 2)
-			return std::max({ heights[lower_index], heights[upper_index], std::min(heights[lower_index], heights[upper_index]) * static_cast<int>(upper_index - lower_index + 1) });
-
-		int min_val = INT_MAX;
-		size_t min_val_index = 0;
-		for (size_t i = lower_index; i <= upper_index; ++i)
-			if (heights[i] < min_val) {
-				min_val = heights[i];
-				min_val_index = i;
-			}
-		return std::max({ min_val * static_cast<int>(upper_index - lower_index + 1), min_val_index == lower_index? 0 : largestRectangleArea(heights, lower_index, min_val_index - 1), min_val_index == upper_index? 0 : largestRectangleArea(heights, min_val_index + 1, upper_index) });
-	}
-
-	int largestRectangleArea(const vector<int>& heights, const size_t lower_index, const size_t upper_index, const multimap<int, size_t> &heights_index_map, const int pre_min) {
-		if (upper_index - lower_index < 2)
-			return std::max({ heights[lower_index], heights[upper_index], std::min(heights[lower_index], heights[upper_index]) * static_cast<int>(upper_index - lower_index + 1) });
-
-		auto map_iter = heights_index_map.cbegin();
-		
-		int min_val = map_iter->first;
-		size_t min_val_index = map_iter->second;
-		// for (size_t i = lower_index; i <= upper_index; ++i)
-		// 	if (heights[i] < min_val) {
-		// 		min_val = heights[i];
-		// 		min_val_index = i;
-		// 	}
-		return std::max({ min_val * static_cast<int>(upper_index - lower_index + 1), min_val_index == lower_index? 0 : largestRectangleArea(heights, lower_index, min_val_index - 1), min_val_index == upper_index? 0 : largestRectangleArea(heights, min_val_index + 1, upper_index) });
+		multimap<int, size_t> heights_index_map;
+		for (size_t i = 0; i < heights.size(); ++i) heights_index_map.insert({ heights[i], i });
+		set<size_t> indexes;
+		int rtn = 0;
+		for (auto map_iter = heights_index_map.cbegin(); map_iter != heights_index_map.cend(); ++map_iter) {
+			auto set_iter = indexes.insert(map_iter->second).first;
+			auto lower_iter = set_iter;
+			const size_t lower_index = lower_iter != indexes.begin() ? (*(--lower_iter)) + 1 : 0;
+			const int area = map_iter->first * (static_cast<int>(++set_iter == indexes.end() ? heights.size() : *(set_iter)) - static_cast<int>(lower_index));
+			if (rtn < area) rtn = area;
+		}
+		return rtn;
 	}
 };
 
 int main(int argc, char const* argv[]) {
+	std::cout << __cplusplus << std::endl;
+
 	Solution sln;
 	int rtn = sln.largestRectangleArea({ 2, 1, 5, 6, 2, 3 });
 	rtn = sln.largestRectangleArea({ 0, 2, 0 });
