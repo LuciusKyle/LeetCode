a9b6b0cacc992809b3d7ecb1a4e7748712de2cc3
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-10-12 15:00:00 +0800

    0011 solved.

--- 0011_Container_with_Most_Water/0011.cc
+++ 0011_Container_with_Most_Water/0011.cc
@@ -2,14 +2,14 @@
 
 #include <assert.h>
 #include <algorithm>  // for std::min().
+#include <vector>
 
 int maxArea(int* height, int heightSize) {
   int left = 0;
   int right = heightSize - 1;
   int max_area = 0;
   while (right > left) {
-    const int current_area =
-        (right - left) * std::min(height[left], height[right]);
+    const int current_area = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
     if (max_area < current_area) max_area = current_area;
     if (height[left] < height[right])
       ++left;
@@ -22,6 +22,32 @@ int maxArea(int* height, int heightSize) {
   }
   return max_area;
 }
+
+using std::vector;
+
+class Solution {
+ public:
+  int maxArea(vector<int>& height) {
+    int left = 0;
+    int right = static_cast<int>(height.size()) - 1;
+    int max_area = 0;
+    while (right > left) {
+      const int current_area = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
+      if (max_area < current_area) max_area = current_area;
+      if (height[left] < height[right]) {
+        ++left;
+        continue;
+      }
+      if (height[left] > height[right]) {
+        --right;
+        continue;
+      }
+      ++left;
+      --right;
+    }
+    return max_area;
+  }
+};
 
 int main(void) {
   int height_array[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
