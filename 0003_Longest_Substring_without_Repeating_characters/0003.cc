5906142b88973e6355101a5654d61ba4c161dec1
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-10-12 10:37:56 +0800

    0003 solved.

--- 0003_Longest_Substring_without_Repeating_characters/0003.cc
+++ 0003_Longest_Substring_without_Repeating_characters/0003.cc
@@ -1,34 +1,31 @@
 
-#include<algorithm>
-#include<string>
-#include<array>
+#include <assert.h>
+#include <algorithm>
+#include <array>
+#include <string>
 
 using std::string;
 
-
-
 class Solution {
-public:
-	int lengthOfLongestSubstring(const string s) {
-		int max_len = 0;
-		int start = -1;
-		for (int i = 0; i < s.size(); ++i) {
-			if (dict[s[i]] > start) {
-				start = dict[s[i]];
-			}
-			dict[s[i]] = i;
-			max_len = std::max(max_len, i - start);
-		}
-		return max_len;
-	}
-private:
-	std::array<int, 255> dict{ 0 };
+ public:
+  int lengthOfLongestSubstring(const string s) {
+    std::array<size_t, 0x100> dict{0};
+    size_t max_len = 0;
+    size_t start = 0;
+    for (size_t i = 1; i <= s.size(); ++i) {
+      if (start < dict[s[i - 1]]) start = dict[s[i - 1]];
+      dict[s[i - 1]] = i;
+      if (max_len < i - start) max_len = i - start;
+    }
+    return static_cast<int>(max_len);
+  }
 };
 
-int main(void)
-{
-	Solution sln;
-	auto rtn = sln.lengthOfLongestSubstring("bbbbbb");
-
-	return 0;
+int main(void) {
+  Solution sln;
+  assert(1 == sln.lengthOfLongestSubstring("a"));
+  assert(1 == sln.lengthOfLongestSubstring("bbbbbb"));
+  assert(3 == sln.lengthOfLongestSubstring("abcabcbb"));
+  assert(3 == sln.lengthOfLongestSubstring("pwwkew"));
+  return 0;
 }
