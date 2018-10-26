79d1dd7dd012b1e0a8749b640d75421b418f9752
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-10-26 16:13:18 +0800

    0201 not finished.

---
+++ 0201_Bitwise_AND_of_Numbers_Range/0201.cc
@@ -0,0 +1,30 @@
+
+#include <array>
+#include <ios>
+#include <iostream>
+
+std::array power_of_2{0x0,       0x2,       0x4,       0x8,        0x10,
+                      0x20,      0x40,      0x80,      0x100,      0x200,
+                      0x400,     0x800,     0x1000,    0x2000,     0x4000,
+                      0x8000,    0x10000,   0x20000,   0x40000,    0x80000,
+                      0x100000,  0x200000,  0x400000,  0x800000,   0x1000000,
+                      0x2000000, 0x4000000, 0x8000000, 0x10000000, 0x20000000,
+                      0x40000000};
+
+
+class Solution {
+ public:
+  int rangeBitwiseAnd(int m, int n) {
+    int base = 2147483647;
+    
+  }
+};
+
+int main(int argc, char const *argv[]) {
+  std::cout << std::hex << 2147483647 << std::endl;
+
+  int a = 1;
+  std::cout<<(a<<2)<<std::endl;
+
+  return 0;
+}
