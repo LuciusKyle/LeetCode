cc20a9ae269519bd1c21c3860843582a3bbe31e9
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-11-06 17:14:23 +0800

    0258 solved.

--- 0258_Add_Digits/0258.cc
+++ 0258_Add_Digits/0258.cc
@@ -4,25 +4,12 @@
 
 class Solution {
  public:
-  int addDigits(int num) {
-    if (num < 10) return num;
-    int result = 0;
-    while (num > 0) {
-      result += num % 10;
-      num /= 10;
-    }
-    return addDigits(result);
-  }
+  int addDigits(int num) { return (num - 1) % 9 + 1; }
 };
 
 int main(void) {
   Solution sln;
   assert(2 == sln.addDigits(38));
-
-  std::ofstream file("out.file");
-  for (int i = 0; i < 0x1000; ++i) {
-    file << i << '\t' << sln.addDigits(i) << '\n';
-  }
 
   return 0;
 }
