cad7689cd6bb183db80c76e8b3ec5bf579348860
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-11-09 18:10:03 +0800

    0295 not finished.

---
+++ 0295_Find_Median_from_Data_Stream/0295.cc
@@ -0,0 +1,23 @@
+
+#include<vector>
+
+using std::vector;
+
+class MedianFinder {
+ public:
+  /** initialize your data structure here. */
+  MedianFinder() {}
+
+  void addNum(int num) {}
+
+  double findMedian() {}
+  private:
+
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
