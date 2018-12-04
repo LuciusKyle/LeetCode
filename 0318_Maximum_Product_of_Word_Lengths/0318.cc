7dbc2168208cf6a48c25c5017d9fc25b339c9e54
Lucius Kyle <23239880+LuciusKyle@users.noreply.github.com>
2018-12-04 11:32:26 +0800

    0318 solved.

--- 0318_Maximum_Product_of_Word_Lengths/0318.cc
+++ 0318_Maximum_Product_of_Word_Lengths/0318.cc
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <array>
 #include <string>
 #include <vector>
@@ -10,33 +11,22 @@ using std::vector;
 class Solution {
  public:
   int maxProduct(const vector<string>& words) {
-    int rtn = 0;
-    for (auto iter = words.cbegin(); iter != words.cend(); ++iter) {
-      array<char, 26> charactor_dict{0};
-      for (const auto ch : *iter) {
-        charactor_dict[ch - 'a'] = 1;
-      }
-      for (auto inner_inter = iter; inner_inter != words.cend();
-           ++inner_inter) {
-        bool no_common_letters = true;
-        for (const auto ch : *inner_inter) {
-          if (charactor_dict[ch - 'a'] == 1) {
-            no_common_letters = false;
-            break;
-          }
-        }
-        if (no_common_letters) {
-          if (rtn < static_cast<int>(iter->size() * inner_inter->size()))
-            rtn = static_cast<int>(iter->size() * inner_inter->size());
-        }
-      }
-    }
-    return rtn;
+    vector<unsigned int> charactor_mask(words.size(), 0u);
+    for (size_t i = 0; i < words.size(); ++i)
+      for (const auto ch : words[i]) charactor_mask[i] |= 1 << (ch - 'a');
+
+    size_t rtn = 0;
+    for (size_t i = 0; i < charactor_mask.size(); ++i)
+      for (size_t ii = i + 1; ii < charactor_mask.size(); ++ii)
+        if ((charactor_mask[i] & charactor_mask[ii]) == 0)
+          rtn = std::max(rtn, (words[i].size() * words[ii].size()));
+
+    return static_cast<int>(rtn);
   }
 };
 
 int main(void) {
   Solution sln;
-  int rtn = sln.maxProduct({"abcw", "baz", "foo", "bar", "xtfn", "abcdef"});
+  assert(16 == sln.maxProduct({"abcw", "baz", "foo", "bar", "xtfn", "abcdef"}));
   return 0;
 }
