46d6015847adb1baa1aa2b49e7046b74b031307a
Lucius Kyle <LuciusKyle@outlook.com>
2019-03-16 18:49:15 +0000

    0345 solved.

--- 0345_Reverse_Vowels_of_a_String/0345.cc
+++ 0345_Reverse_Vowels_of_a_String/0345.cc
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <string>
 
 using std::string;
@@ -9,17 +10,29 @@ class Solution {
     size_t begin_index = 0;
     size_t end_index = s.empty() ? s.size() : s.size() - 1;
     while (begin_index < end_index) {
-      
+      if (isVowel(s[begin_index]))
+        for (; begin_index < end_index; --end_index)
+          if (isVowel(s[end_index])) {
+            s[begin_index] ^= s[end_index];
+            s[end_index] ^= s[begin_index];
+            s[begin_index] ^= s[end_index];
+            --end_index;
+            break;
+          }
+      ++begin_index;
     }
+    return s;
   }
-  private:
-  bool isVowel(const char ch){
+
+ private:
+  bool isVowel(const char ch) {
     return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
   }
 };
 
-int main(void){
-
+int main(void) {
+  Solution sln;
+  assert("leotcede" == sln.reverseVowels("leetcode"));
   return 0;
 }
