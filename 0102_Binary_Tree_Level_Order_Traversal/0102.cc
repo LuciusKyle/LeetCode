7c6c0d022d6c01815d773ce43345a4817275e3f8
Lucius Kyle <LuciusKyle@outlook.com>
2019-03-22 20:19:50 +0000

    0102 solved.

---
+++ 0102_Binary_Tree_Level_Order_Traversal/0102.cc
@@ -0,0 +1,52 @@
+
+#include <vector>
+
+using std::vector;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+ public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    if (root == nullptr) return vector<vector<int>>();
+    vector<vector<int>> rtn;
+    rtn.push_back(vector<int>(1, root->val));
+    vector<TreeNode *> cur_level(1, root);
+    vector<TreeNode *> next_level;
+    do {
+      next_level.clear();
+      for (size_t i = 0; i < cur_level.size(); ++i) {
+        if (cur_level[i]->left != nullptr)
+          next_level.push_back(cur_level[i]->left);
+        if (cur_level[i]->right != nullptr)
+          next_level.push_back(cur_level[i]->right);
+      }
+      if (!next_level.empty()) {
+        rtn.push_back(vector<int>());
+        for (const auto ptr : next_level) rtn.back().push_back(ptr->val);
+        cur_level = next_level;
+      }
+    } while (!next_level.empty());
+    return rtn;
+  }
+};
+
+int main(void) {
+  TreeNode root(1);
+  TreeNode t1(2);
+  TreeNode t2(3);
+
+  root.left = &t1;
+  // root.right = &t2;
+
+  Solution sln;
+  auto rtn = sln.levelOrder(&root);
+  
+  return 0;
+}
