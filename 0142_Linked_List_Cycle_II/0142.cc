a9fa91f0153a3c82cfe7c74f136c9317edca4e45
Lucius Kyle <LuciusKyle@outlook.com>
2019-03-23 17:11:47 +0000

    0142 solved.

--- 0142_Linked_List_Cycle_II/0142.cc
+++ 0142_Linked_List_Cycle_II/0142.cc
@@ -10,6 +10,7 @@ struct ListNode {
   ListNode(int x) : val(x), next(nullptr) {}
 };
 
+// use hash table.
 class Solution {
  public:
   ListNode *detectCycle(ListNode *head) {
@@ -23,7 +24,6 @@ class Solution {
   }
 
  private:
-  
   // leetcode problem 0141.
   bool hasCycle(ListNode *head) {
     unordered_set<ListNode *> ptrs;
@@ -33,5 +33,25 @@ class Solution {
       else
         return true;
     return false;
+  }
+};
+
+// use two pointer.
+class Solution2 {
+ public:
+  ListNode *detectCycle(ListNode *head) {
+    ListNode *fast_ptr = head;
+    ListNode *slow_ptr = head;
+    while (true) {
+      if (fast_ptr == nullptr || fast_ptr->next == nullptr) return nullptr;
+      fast_ptr = fast_ptr->next->next;
+      slow_ptr = slow_ptr->next;
+      if (fast_ptr == slow_ptr) break;
+    }
+    while (head != slow_ptr) {
+      head = head->next;
+      slow_ptr = slow_ptr->next;
+    }
+    return slow_ptr;  // never reach here.
   }
 };
