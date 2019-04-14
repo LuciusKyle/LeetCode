9de80ec380a0fe7bd39c0222eebbac02e72cbf4d
Lucius Kyle <LuciusKyle@outlook.com>
2019-04-14 19:49:17 +0000

    0033 solved.

--- 0033_Search_in_Rotated_Sorted_Array/0033.cc
+++ 0033_Search_in_Rotated_Sorted_Array/0033.cc
@@ -1,100 +1,42 @@
 
+#include <limits.h>
 #include <vector>
-#include<limits.h>
 
 using std::vector;
 
-int search(vector<int>& nums, int target) {
-    int lo = 0, hi = nums.size();
-    while (lo < hi) {
-        int mid = (lo + hi) / 2;
-        
-        double num = (nums[mid] < nums[0]) == (target < nums[0])
-                   ? nums[mid]
-                   : target < nums[0] ? -INFINITY : INFINITY;
-                   
-        if (num < target)
-            lo = mid + 1;
-        else if (num > target)
-            hi = mid;
-        else
-            return mid;
-    }
-    return -1;
-}
-
-int binary_search(const int* nums, const int numsSize, const int target) {
-  if (numsSize < 4) {
-    for (int i = 0; i < numsSize; ++i)
-      if (nums[i] == target) return i;
-    return -1;
-  }
-  const int mid_index = numsSize / 2;
-  const int mid_val = nums[mid_index];
-  if (target < mid_val)
-    return binary_search(nums, mid_index, target);
-  else if (mid_val < target)
-    return binary_search(nums + mid_index, numsSize - mid_index, target);
-  else
-    return mid_index;
-}
-
-// wrong answer, this fuction did find the number, but will not return the correct array index.
-int search_wrong(int* nums, int numsSize, int target) {
-  if (numsSize < 4) {
-    for (int i = 0; i < numsSize; ++i)
-      if (nums[i] == target) return i;
-    return -1;
-  }
-  
-  const int mid_index = numsSize / 2;
-  const int mid_val = nums[mid_index - 1];
-  if (nums[0] < mid_val)  // left half is ordered.
-  {
-    if (nums[0] <= target && target <= mid_val) {
-      // target is in left half.
-      return search(nums, mid_index, target);
-    }
-    else{
-      // target is in right half.
-      return search(nums + mid_index, numsSize - mid_index, target);
-    }
-  } else if (mid_val < nums[numsSize - 1])  // right half is ordered.
-  {
-    if (mid_val <= target && target <= nums[numsSize - 1]) {
-      // target is in left half.
-      return search(nums + mid_index, numsSize - mid_index, target);
-    } else {
-      // target is in right half.
-      return search(nums, mid_index, target);
-    }
-  }
-}
-
-int search(int* nums, int numsSize, int target){
-  int lower_index = 0;
-  int upper_index = numsSize -1;
-
-  int target_index = -1;
-  while(upper_index - lower_index > 1){
-    if(upper_index - lower_index < 3){
-      for(int i = lower_index; i < upper_index; ++i){
-        if(nums[i]==target)
-        target_index = i;
+class Solution {
+ public:
+  int search(const vector<int>& nums, int target) {
+    if (nums.empty()) return -1;
+    size_t lower_index = 0, upper_index = nums.size() - 1;
+    while (lower_index + 1 < upper_index) {
+      const size_t mid_index = lower_index / 2 + upper_index / 2 + ((0 != (0x1 & lower_index) && 0 != (0x1 & upper_index)) ? 1 : 0);
+      if (nums[lower_index] < nums[mid_index]) { // left part is sorted.
+        if (nums[lower_index] <= target && target <= nums[mid_index]) {
+          upper_index = mid_index;
+        }
+        else {
+          lower_index = mid_index;
+        }
+      }
+      else { //right part is sorted.
+        if (nums[mid_index] <= target && target <= nums[upper_index]) {
+          lower_index = mid_index;
+        }
+        else {
+          upper_index = mid_index;
+        }
       }
     }
-    const int mid_index = (upper_index + lower_index) / 2;
-    if(nums[mid_index] <= nums[lower_index]) // left half is ordered.
-    {
-      if()
-    }
-    else if (nums[0] <= )
-  }
-  return target_index;
-}
 
-int main(void){
-  int arr[] = {4,5,6,7,0,1,2};
-  int rtn = search(arr, std::end(arr) - std::begin(arr), 2);
+    if (nums[lower_index] == target) return int(lower_index);
+    else if (nums[upper_index] == target) return int(upper_index);
+    else return -1;
+  }
+};
+
+int main(void) {
+  Solution sln;
+  int rtn = sln.search({4, 5, 6, 7, 0, 1, 2, 3}, 0);
   return 0;
 }
--- 0322_Coin_Change/0322.cc
+++ 0322_Coin_Change/0322.cc
@@ -1,43 +1,53 @@
 
+#include <assert.h>
 #include <limits.h>
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 
-using std::unordered_map;
 using std::vector;
 
 class Solution {
  public:
   int coinChange(const vector<int>& coins, int amount) {
-    if (amount == 0) return 0;
-    int rtn = coinChange_helper(coins, amount);
-    return rtn == INT_MAX ? -1 : rtn;
+    if (coins.empty()) return -1;
+    vector<int> dp(amount + 1, -1);
+    dp[0] = 0;
+    for (int i = 1; i < dp.size() && i < amount; ++i) {
+      int min = -1;
+      for (const int coin : coins) {
+        if (i - coin < 0) continue;
+        if (dp[i - coin] != -1 && dp[i - coin] + 1 < min)
+          min = dp[i - coin] + 1;
+      }
+      dp[i] = min;
+    }
+
   }
 
  private:
-  int coinChange_helper(const vector<int>& coins, const int amount) {
-    if (dp_.count(amount) != 0) return dp_.at(amount);
-    int rtn = INT_MAX;
-    for (const int coin : coins) {
-      if (0 < amount - coin) {
-        if (coinChange_helper(coins, amount - coin) < rtn - 1)
-          rtn = 1 + dp_.at(amount - coin);
-      } else if (amount - coin < 0)
-        continue;
-      else {
-        dp_.insert({amount, 1});
-        return 1;
-      }
-    }
-    if (rtn != INT_MAX) dp_.insert({amount, rtn});
-    return rtn;
-  }
 
-  unordered_map<int, int> dp_;
+  // not used in this case.
+  int LeastCommonMultiple(const vector<int>& coins, int max_coin) {
+    if (coins.empty()) return 0;
+    int multiple_time = 1;
+    bool keep_running = true;
+    while (keep_running) {
+      keep_running = false;
+      for (const int coin : coins)
+        if ((max_coin * multiple_time) % coin != 0) {
+          ++multiple_time;
+          keep_running = true;
+          break;
+        }
+    }
+    return max_coin * multiple_time;
+  }
 };
 
 int main(void) {
   Solution sln;
-
+  // int rtn = sln.coinChange({186, 419, 83, 408}, 6249);
+  // assert(3 == sln.coinChange({1, 2, 5}, 11));
+  int rtn = sln.coinChange({2}, 3);
   return 0;
 }
