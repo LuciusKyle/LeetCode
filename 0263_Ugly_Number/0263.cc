fcbde655ea0f96da4aa9be561201cda87bd836a4
Lucius Kyle <LuciusKyle@outlook.com>
2019-04-16 04:16:26 +0000

    0263 solved.

--- 0263_Ugly_Number/0263.cc
+++ 0263_Ugly_Number/0263.cc
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <limits.h>
 #include <array>
 #include <unordered_set>
@@ -8,9 +9,21 @@ using std::vector;
 
 constexpr std::array base_arr{2, 3, 5};
 
+// my final solution.
 class Solution {
  public:
-  Solution() : ugly_num_set_{1, 2, 3, 5} {}
+  bool isUgly(int num) {
+    if (num <= 0) return false;
+    while (num % 2 == 0) num /= 2;
+    while (num % 3 == 0) num /= 3;
+    while (num % 5 == 0) num /= 5;
+    return num == 1;
+  }
+};
+
+class Solution1 {
+ public:
+  Solution1() : ugly_num_set_{1, 2, 3, 5} {}
   bool isUgly(int num) {
     if (num <= 0) return false;
     if (0 != ugly_num_set_.count(num)) return true;
@@ -29,6 +42,12 @@ class Solution {
 
 int main(void) {
   Solution sln;
-  sln.isUgly(14);
+  Solution1 sln1;
+  for (int i = -1; i < 0x1000; ++i) {
+    if (sln1.isUgly(i))
+      assert(sln.isUgly(i));
+    else
+      assert(!sln.isUgly(i));
+  }
   return 0;
 }
---
+++ 0264_Ugly_Number_II/0264.cc
@@ -0,0 +1,21 @@
+
+#include <array>
+#include <set>
+#include <vector>
+
+using std::set;
+
+constexpr std::array base_factor{2, 3, 5};
+
+class Solution {
+ public:
+  int nthUglyNumber(int n) {
+    
+
+  }
+};
+
+int main(void) {
+  Solution sln;
+  return 0;
+}
