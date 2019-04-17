3e0dd71683071b59e816ee2cb84d25c157746c0a
Lucius Kyle <LuciusKyle@outlook.com>
2019-04-17 05:05:35 +0000

    0322 solved.

--- 0322_Coin_Change/0322.cc
+++ 0322_Coin_Change/0322.cc
@@ -10,18 +10,17 @@ class Solution {
  public:
   int coinChange(const vector<int>& coins, int amount) {
     if (coins.empty()) return -1;
-    vector<int> dp(amount + 1, -1);
-    dp[0] = 0;
-    for (int i = 1; i < dp.size() && i < amount; ++i) {
-      int min = -1;
-      for (const int coin : coins) {
-        if (i - coin < 0) continue;
-        if (dp[i - coin] != -1 && dp[i - coin] + 1 < min)
-          min = dp[i - coin] + 1;
+    vector<int> dp(1, 0);
+    for (int i = 1; i < amount + 1; ++i) {
+      dp.push_back(-1);
+      int min_coin_count = INT_MAX;
+      for (const int coin:coins) {
+        if (i - coin < 0 || dp[i - coin] == -1) continue;
+        if (dp[i - coin] < min_coin_count) min_coin_count = dp[i - coin];
       }
-      dp[i] = min;
+      if (INT_MAX != min_coin_count) dp.back() = min_coin_count + 1;
     }
-
+    return dp.back();
   }
 
  private:
@@ -47,7 +46,7 @@ class Solution {
 int main(void) {
   Solution sln;
   // int rtn = sln.coinChange({186, 419, 83, 408}, 6249);
-  // assert(3 == sln.coinChange({1, 2, 5}, 11));
+  assert(3 == sln.coinChange({1, 2, 5}, 11));
   int rtn = sln.coinChange({2}, 3);
   return 0;
 }
