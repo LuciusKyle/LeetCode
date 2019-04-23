a1ea5cea2802c58151b5ed109d0690a22aa11947
Lucius Kyle <LuciusKyle@outlook.com>
2019-04-23 14:40:43 +0000

    0084 18% beat.

--- 0084_Largest_Rectangle_in_Histogram/0084.cc
+++ 0084_Largest_Rectangle_in_Histogram/0084.cc
@@ -1,16 +1,19 @@
-#include <iostream>
-
+#include <assert.h>
 #include <limits.h>
 #include <algorithm>
+#include <iostream>
 #include <map>
+#include <random>
 #include <set>
+#include <stack>
 #include <vector>
 
 using std::multimap;
 using std::set;
 using std::vector;
 
-class Solution {
+// 18% beat.
+class Solution_old {
 public:
 	int largestRectangleArea(const vector<int>& heights) {
 		multimap<int, size_t> heights_index_map;
@@ -28,12 +31,52 @@ public:
 	}
 };
 
-int main(int argc, char const* argv[]) {
-	std::cout << __cplusplus << std::endl;
+// not finished.
+class Solution {
+ public:
+  int largestRectangleArea(const vector<int>& heights) {
+    std::stack<size_t> stk;
+    int max_area = 0;
+    for (size_t i = 0; i < heights.size(); ++i) {
+      if (stk.empty() || heights[stk.top()] < heights[i]) {
+        stk.push(i);
+      } else {
+        while (!stk.empty() && heights[i] <= heights[stk.top()]) {
+          const int cur_area = heights[stk.top()] * int(i - stk.top());
+          if (max_area < cur_area) max_area = cur_area;
+          stk.pop();
+        }
+        stk.push(i);
+      }
+    }
+    while (!stk.empty()) {
+      const int cur_area = heights[stk.top()] * int(heights.size() - stk.top());
+      if (max_area < cur_area) max_area = cur_area;
+			stk.pop();
+    }
+    return max_area;
+  }
+};
 
-	Solution sln;
-	int rtn = sln.largestRectangleArea({ 2, 1, 5, 6, 2, 3 });
-	rtn = sln.largestRectangleArea({ 0, 2, 0 });
-	rtn = sln.largestRectangleArea({ 2, 1, 2 });
-	return 0;
+int main(int argc, char const* argv[]) {
+  std::cout << __cplusplus << std::endl;
+
+  Solution sln;
+  int rtn = sln.largestRectangleArea({2, 1, 5, 6, 2, 3});
+  rtn = sln.largestRectangleArea({0, 2, 0});
+  rtn = sln.largestRectangleArea({2, 1, 2});
+
+  Solution_old old_sln;
+  std::random_device rd;
+  std::uniform_int_distribution<int> dist(0, 0x1000);
+  for (size_t test_count = 0; test_count < 10000; ++test_count) {
+    vector test_sample(dist(rd), 0);
+    for (size_t i = 0; i < test_sample.size(); ++i) test_sample[i] = dist(rd);
+		int result = old_sln.largestRectangleArea(test_sample);
+    int result1 = sln.largestRectangleArea(test_sample);
+		assert(old_sln.largestRectangleArea(test_sample) == sln.largestRectangleArea(test_sample));
+    std::cout << test_count << std::endl;
+  }
+
+  return 0;
 }
