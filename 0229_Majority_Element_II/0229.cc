bdb548edeed13f285e993106dd63d68c410426ca
Lucius Kyle <LuciusKyle@outlook.com>
2020-03-09 08:20:22 +0000

    0416 5% beat.

---
+++ 0229_Majority_Element_II/0229.cc
@@ -0,0 +1,14 @@
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+ public:
+  vector<int> majorityElement(vector<int>& nums) {
+    vector<int> rtn;
+    if (nums.empty()) return rtn;
+    
+  }
+};
+
--- 0416_Partition_Equal_Subset_Sum/0416.cc
+++ 0416_Partition_Equal_Subset_Sum/0416.cc
@@ -1,24 +1,39 @@
 
 #include <vector>
+#include <map>
 
 using std::vector;
+using std::map;
 
 class Solution {
  public:
   bool canPartition(const vector<int>& nums) {
+    vector<map<int, bool>> result(nums.size());
     int target = 0;
-    for (const int num : nums) target += num;
-    if (target % 2 != 0) return false;
-    target /= 2;
-    
+    int odd_count = 0;
+    for (const int num : nums) {
+      target += num / 2;
+      if (num % 2) ++odd_count;      
+    }
+    if (odd_count % 2 == 1 || nums.size() < 2) return false;
+    target += odd_count / 2;
+    return canPartition(nums, 0, target, result);
   }
 
  private:
-  bool canPartition(const vector<int>& nums, size_t cur_index, int target);
-  
+  bool canPartition(const vector<int>& nums, const size_t cur_index, const int target, vector<map<int, bool>>& result) {
+    // if (nums.empty()) return false; // don't check empty at here.
+    if (nums.size() - cur_index == 0) return false;
+    if (nums.size() - cur_index == 1) return nums[cur_index] == target;
+    if (result[cur_index].find(target) != result[cur_index].end()) return result[cur_index].at(target);
+    const bool rtn = canPartition(nums, cur_index + 1, target, result) || canPartition(nums, cur_index + 1, target - nums[cur_index], result);
+    result[cur_index].insert({target, rtn});
+    return rtn;
+  }
 };
 
 int main(void) {
   Solution sln;
   return 0;
 }
+
