075cf662499fe4a032ba73e56c1ba4ab5aba7600
Lucius Kyle <LuciusKyle@outlook.com>
2020-04-10 17:04:05 +0000

    0039 solved.

--- 0039_Combination_Sum/0039.cc
+++ 0039_Combination_Sum/0039.cc
@@ -1,59 +1,37 @@
 
-#include <unordered_set>
 #include <vector>
+#include <algorithm>
+#include <set>
 
-using std::unordered_set;
 using std::vector;
+
 
 class Solution {
  public:
-  vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+  vector<vector<int>> combinationSum(const vector<int>& candidates, int target) {
     vector<vector<int>> rtn;
-    if (!candidates.empty()) {
-      vector<int> answer_buffer;
-      answer_buffer.reserve(candidates[0] * target);
-      sub_combinationSum(candidates, target, candidates.size() - 1,
-                         answer_buffer, rtn);
-    }
+    vector<int> answer_buffer;
+    combinationSum(candidates, target, 0, rtn, answer_buffer);
     return rtn;
   }
 
  private:
-  int sub_combinationSum(const vector<int>& candidates, const int target,
-                         const size_t current_index, vector<int>& answer,
-                         vector<vector<int>>& rtn) {
-    if (current_index == 0) {
-      if (target % candidates[0] != 0)
-        return -1;
-      else {
-        const int count = target / candidates[0];
-        for (int i = 0; i < count; ++i) answer.push_back(candidates[0]);
-        rtn.push_back(answer);
-        for (int i = 0; i < count; ++i) answer.pop_back();
-        return count;
+  void combinationSum(const vector<int>& candidates, const int target, const size_t start_index, vector<vector<int>>& rtn, vector<int> answer_buffer) {
+    for (size_t i = start_index; i < candidates.size(); ++i) {
+      if (0 < target - candidates[i]) {
+        answer_buffer.push_back(candidates[i]);
+        combinationSum(candidates, target - candidates[i], i, rtn, answer_buffer);
+        answer_buffer.pop_back();
+        continue;
+      }
+      if (target - candidates[i] == 0) {
+        answer_buffer.push_back(candidates[i]);
+        rtn.push_back(answer_buffer);
+        answer_buffer.pop_back();
       }
     }
-    int count = target / candidates[current_index] + 1;
-    for (int i = 0; i < count; ++i) {
-      for (int ii = 0; ii < i; ++ii)
-        answer.push_back(candidates[current_index]);
-      if (-1 != sub_combinationSum(candidates,
-                                   target - i * candidates[current_index],
-                                   current_index - 1, answer, rtn)) {
-        // return target / candidates[current_index];
-
-        // can't return function here. continue seearching for all possible
-        // answer.
-      } else {
-      }
-      // return -1;
-
-      for (int ii = 0; ii < i; ++ii) answer.pop_back();
-    }
-    return -1;
   }
 };
-
 
 
 int main(void) {
