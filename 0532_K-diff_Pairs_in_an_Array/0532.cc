2605624858eed49c86f073c6d61a0edf9014d018
Lucius Kyle <LuciusKyle@outlook.com>
2020-04-14 05:32:18 +0000

    0532 not finished.

---
+++ 0532_K-diff_Pairs_in_an_Array/0532.cc
@@ -0,0 +1,18 @@
+
+#include <unordered_set>
+#include <vector>
+
+using std::unordered_set;
+using std::vector;
+
+class Solution {
+ public:
+  int findPairs(vector<int>& nums, int k) {
+    
+  }
+};
+
+int main(void) {
+
+  return 0;
+}
