70ba17bc5f5a41492d0e50814d315a41193f48ac
Lucius Kyle <LuciusKyle@outlook.com>
2020-04-14 15:21:22 +0000

    0554 solved.

--- 0554_Brick_Wall/0554.cc
+++ 0554_Brick_Wall/0554.cc
@@ -2,41 +2,30 @@
 #include <limits.h>
 
 #include <chrono>
-#include <array>
 #include <iostream>
+#include <unordered_map>
 #include <numeric>
 #include <vector>
 
 using std::vector;
+using std::unordered_map;
 
 class Solution {
  public:
   int leastBricks(const vector<vector<int>>& wall) {
-    vector<std::array<int, 2>> pre_wall;
-    const int wall_hight = std::accumulate(wall[0].cbegin(), wall[0].cend(), 0);
-    for (int i = 0; i < wall.size(); ++i) {
-      int pos = 0, it = 0;
-      vector<std::array<int, 2>> cur_wall;
-      for (int j = 0; j < wall[i].size(); ++j) {
-        pos += wall[i][j];        
-        while (it != pre_wall.size() && pre_wall[it][0] < pos) {
-          cur_wall.push_back({pre_wall[it][0], pre_wall[it][1] + 1});
-          ++it;
-        }
-
-        if (it != pre_wall.size() && pre_wall[it][0] == pos) {
-          cur_wall.push_back({pos, pre_wall[it][1]});
-          ++it;
-        } else
-          cur_wall.push_back({pos, i});
+    unordered_map<int, int> brick_edge_count;
+    for (const auto& vec : wall) {
+      int pos = 0;
+      for (int i = 0; i < vec.size() - 1; ++i) {
+        pos += vec[i];
+        if (brick_edge_count.count(pos)) ++brick_edge_count.at(pos);
+        else brick_edge_count.insert({pos, 1});
       }
-      pre_wall = cur_wall;
     }
-    int minBrick = INT_MAX;
-    if (pre_wall.size() == 1 && pre_wall[0][0] == wall_hight) minBrick = wall.size();
-    for (const auto& val : pre_wall)
-      if (val[1] < minBrick) minBrick = val[0] == wall_hight ? minBrick : val[1];
-    return minBrick;
+    int most_common = 0;
+    for (auto iter = brick_edge_count.cbegin(); iter != brick_edge_count.cend(); ++iter)
+      if (most_common < iter->second) most_common = iter->second;
+    return wall.size() - most_common;
   }
 };
 
@@ -48,7 +37,7 @@ int main(void) {
   vector<vector<int>> test_sample;
 
   auto start = std::chrono::system_clock::now();
-  for (int i = 0; i < 10000; ++i) test_sample.push_back({i, INT_MAX - i});
+  for (int i = 0; i < 1000000; ++i) test_sample.push_back({i, INT_MAX - i});
     
   std::cout << "time to construct sample: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(
