2faa5dbd4346b93fcce2e5fdbc805b9e123f1a10
Lucius Kyle <LuciusKyle@outlook.com>
2020-04-20 16:55:14 +0000

    0561 solved.

--- 0561_Array_Partition_I/0561.cc
+++ 0561_Array_Partition_I/0561.cc
@@ -5,7 +5,8 @@
 
 using std::vector;
 
-class Solution {
+// 48% beat.
+class Solution1 {
  public:
   int arrayPairSum(vector<int>& nums) {
     // std::sort(std::execution::par, nums.begin(), nums.end());
@@ -14,6 +15,31 @@ class Solution {
     for (int i = 0; i < nums.size(); i += 2) ret += nums[i];
 
     return ret;
+  }
+};
+
+// 96% beat.
+class Solution {
+ public:
+  int arrayPairSum(const vector<int>& nums) {
+    int* counter = new int[20001]{0};
+    for (const int num : nums) ++counter[10000 + num];
+
+    int sum = 0;
+    bool counter_odd = true;
+    for (int i = 0; i < 20001; ++i) {
+      const int val = counter[i];
+      if (0 != val) {
+        if (counter_odd)
+          sum += (i - 10000) * ((val + 1) / 2);
+        else
+          sum += (i - 10000) * (val / 2);
+        if (val % 2) counter_odd = !counter_odd;
+      }
+    }
+
+    delete[] counter;
+    return sum;
   }
 };
 
