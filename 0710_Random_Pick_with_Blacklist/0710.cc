78028c2e8ef108332234e9fc0ed9e08bad87a400
LuciusKyle <LuciusKyle@outlook.com>
2021-03-11 05:12:56 +0000

    0710 solved.

--- 0710_Random_Pick_with_Blacklist/0710.cc
+++ 0710_Random_Pick_with_Blacklist/0710.cc
@@ -1,29 +1,43 @@
 
-#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <map>
 #include <random>
+#include <vector>
 
+using std::map;
 using std::vector;
+using std::cout;
+using std::endl;
 
 class Solution {
  public:
-  Solution(const int N, const vector<int>& blacklist) : e(rd()), dist(0, N - blacklist.size() - 1), blacklist_(blacklist) {
-    std::sort(blacklist_);
+  Solution(const int N, vector<int>& blacklist) : e(rd()), dist(0, N - blacklist.size() - 1) {
+    std::sort(blacklist.begin(), blacklist.end());
+    dict_.insert({-1, 0});
+    for (int i = 0, pre_num = -1; i < blacklist.size(); ++i) {
+      if (blacklist[i] - 1 == pre_num)
+        dict_.rbegin()->second++;
+      else
+        dict_.insert({blacklist[i] - i, i + 1});
+
+      pre_num = blacklist[i];
+    }
+    dict_.insert({N, N});
   }
 
   int pick() {
     const int ret = dist(e);
-    auto iter = std::lower_bound(blacklist_.cbegin(), blacklist_.cend(), ret);
-
-    if (iter == blacklist_.cend()) return ret + (iter - blacklist_.cbegin());
-    
+    auto iter = dict_.upper_bound(ret);
+    --iter;
+    return ret + iter->second;
   }
 
-    private:
-  
+ private:
   std::random_device rd;
   std::default_random_engine e;
-  const std::uniform_int_distribution<int> dist;
-  vector<int>blacklist_;
+  std::uniform_int_distribution<int> dist;
+  map<int, int> dict_;
 };
 
 /**
@@ -31,3 +45,12 @@ class Solution {
  * Solution* obj = new Solution(N, blacklist);
  * int param_1 = obj->pick();
  */
+
+int main(int argc, char* argv[]) {
+  vector<int> test_blacklist{1};
+  Solution sln(3, test_blacklist);
+  for (int i = 0; i < 0x2f; ++i) {
+    cout << sln.pick() << endl;
+  }
+  return 0;
+}
