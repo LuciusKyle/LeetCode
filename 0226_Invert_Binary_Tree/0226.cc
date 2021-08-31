4b1f63cb8e3e0d977addbd29ed194e1596e2ec58
Lucius Kyle <LuciusKyle@outlook.com>
2021-08-31 09:07:27 +0000

    0226 100% beat!.

--- 0226_Invert_Binary_Tree/0226.cc
+++ 0226_Invert_Binary_Tree/0226.cc
@@ -11,17 +11,19 @@ struct TreeNode {
 class Solution {
  public:
   TreeNode* invertTree(TreeNode* root) {
-    invertNode(root);
+    if (root != nullptr) invertNode(root);
     return root;
   }
 
  private:
-  void invertNode(TreeNode *node) {
-    if (node == nullptr) return;
-    invertNode(node->left);
-    invertNode(node->right);
-    TreeNode *temp_ptr = node->left;
+  void invertNode(TreeNode* node) {
+    TreeNode* temp_ptr = node->left;
     node->left = node->right;
     node->right = temp_ptr;
+
+    if (node->left != nullptr)
+      invertNode(node->left);
+    if (node->right != nullptr)
+      invertNode(node->right);
   }
 };
