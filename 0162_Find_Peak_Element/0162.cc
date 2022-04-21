91aef063570a3b9985c2b525b393390576e19028
LuciusKyle <LuciusKyle@outlook.com>
2022-04-21 06:42:22 +0000

    0162 O(n) solution.

--- 0162_Find_Peak_Element/0162.cc
+++ 0162_Find_Peak_Element/0162.cc
@@ -1,4 +1,6 @@
 
+#include <assert.h>
+
 #include <vector>
 
 using std::vector;
@@ -6,37 +8,21 @@ using std::vector;
 class Solution {
  public:
   int findPeakElement(const vector<int>& nums) {
-    size_t lower_index = 0;
-    size_t upper_index = nums.size() - 1;
+    if (nums.size() == 1) return 0;
+    if (nums[1] < nums[0]) return 0;
+    if (nums[nums.size() - 2] < nums[nums.size() - 1]) return nums.size() - 1;
+    for (size_t i = 1; i < nums.size() - 1; ++i)
+      if (nums[i - 1] < nums[i] && nums[i + 1] < nums[i]) return i;
 
-    if (isPeak(lower_index, nums)) return lower_index;
-    if (isPeak(upper_index, nums)) return upper_index;
-    while (upper_index - lower_index > 1) {
-      const size_t mid_index = (lower_index + upper_index) / 2;
-      if (isPeak(mid_index, nums)) return mid_index;
-      if (nums[mid_index - 1] < nums[mid_index])
-        lower_index = mid_index;
-      else
-        upper_index = mid_index;
-    }
-    if (isPeak(lower_index, nums)) return lower_index;
-    if (isPeak(upper_index, nums)) return upper_index;
-    return 0;
-  }
-
- private:
-  bool isPeak(const size_t index, const vector<int>& nums) {
-    if (index == 0) return nums[1] < nums[0];
-    if (index == nums.size() - 1) return nums[index - 1] < nums[index];
-    return nums[index - 1] < nums[index] && nums[index + 1] < nums[index];
+    return -1;
   }
 };
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const* argv[]) {
   Solution sln;
   int rtn = sln.findPeakElement({0, 1});
+  assert(rtn == 1);
+  rtn = sln.findPeakElement({1, 2, 3, 1});
+  assert(rtn == 2);
   return 0;
 }
-
-
