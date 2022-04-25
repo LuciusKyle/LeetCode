58913c27a791a453f85534c29f4f98294f40fb7a
Lucius Kyle <luciuskyle@outlook.com>
2022-04-25 02:35:07 +0000

    0005 beat 82%.

--- 0005_Longest_Palindromic_Substring/0005.cc
+++ 0005_Longest_Palindromic_Substring/0005.cc
@@ -1,58 +1,45 @@
 
 #include <string>
+#include <vector>
+#include <limits.h>
 
 using std::string;
+using std::vector;
 
 class Solution {
  public:
-  string longestPalindrome(string s) {
-    int max_length = 0;
-    int start_pos = 0;
-    for (int /*can't use unsigned type*/ i = 0; i < s.size(); ++i) {
-      int left_cursor = i, right_cursor = i;
-      int palindrome_length = 0;
-      bool found_palindrome = false;
-      while (left_cursor >= 0 && right_cursor < s.size()) {
-        if (s[left_cursor] == s[right_cursor])
-          palindrome_length = right_cursor - left_cursor + 1;
-        else {
-          --left_cursor;
-          ++right_cursor;
-          break;
-        }
-        --left_cursor;
-        ++right_cursor;
+  string longestPalindrome(const string s) {
+    int longest_palindrome = INT_MIN;
+    int start_pos = 0, end_pos = 0;
+    for (int i = 0; i < s.size(); ++i) {
+      int l_pos = i, r_pos = i;
+      while (0 <= l_pos && r_pos < s.size() && s[l_pos] == s[r_pos]) {
+        --l_pos;
+        ++r_pos;
       }
-      if (palindrome_length > max_length) {
-        max_length = palindrome_length;
-        start_pos = left_cursor + 1;
+      if (longest_palindrome < r_pos - l_pos) {
+        start_pos = l_pos + 1;
+        end_pos = r_pos;
+        longest_palindrome = r_pos - l_pos;
       }
-      left_cursor = i;
-      right_cursor = i + 1;
-      found_palindrome = false;
-      while (left_cursor >= 0 && right_cursor < s.size()) {
-        if (s[left_cursor] == s[right_cursor])
-          palindrome_length = right_cursor - left_cursor + 1;
-        else {
-          --left_cursor;
-          ++right_cursor;
-          break;
-        }
-        --left_cursor;
-        ++right_cursor;
+      l_pos = i, r_pos = i + 1;
+      while (0 <= l_pos && r_pos < s.size() && s[l_pos] == s[r_pos]) {
+        --l_pos;
+        ++r_pos;
       }
-      if (palindrome_length > max_length) {
-        max_length = palindrome_length;
-        start_pos = left_cursor + 1;
+      if (longest_palindrome < r_pos - l_pos) {
+        start_pos = l_pos + 1;
+        end_pos = r_pos;
+        longest_palindrome = r_pos - l_pos;
       }
     }
-    return string(s.begin() + start_pos, s.begin() + start_pos + max_length);
+    return {s.cbegin() + start_pos, s.cbegin() + end_pos};
   }
 };
 
 int main(void) {
   Solution sln;
-  auto rtn = sln.longestPalindrome("abaqwerabcfcba");
+  string rtn = sln.longestPalindrome("abaqwerabcfcba");
   rtn = sln.longestPalindrome("abaqwerabba");
   rtn = sln.longestPalindrome("abaqweraa");
   rtn = sln.longestPalindrome("a");
