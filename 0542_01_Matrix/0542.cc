3bce284e07c2477a0e0f646030e64777588b17e8
LuciusKyle <LuciusKyle@outlook.com>
2022-05-12 13:23:57 +0000

    0542 beat 75%.

--- 0542_01_Matrix/0542.cc
+++ 0542_01_Matrix/0542.cc
@@ -1,61 +1,58 @@
 
+#include <assert.h>
+
 #include <vector>
 
 using std::vector;
 
-// no error on Linux GCC 10. runtime error on LeetCode.
 class Solution {
  public:
   vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-    vector<int> rows, columns, next_rows, next_columns;
-    for (int i = 0; i < matrix.size(); ++i)
-      for (int j = 0; j < matrix[0].size(); ++j)
-        if (matrix[i][j] == 0) {
-          rows.push_back(i);
-          columns.push_back(j);
-        } else
-          matrix[i][j] = -1;
-
-    const int elements_count = matrix.size() * matrix[0].size();
-    int visited = rows.size();
-    while (visited != elements_count) {
-      for (int i = 0; i < rows.size(); ++i) {
-        if (rows[i] != 0 && matrix[rows[i] - 1][columns[i]] == -1) {
-          next_rows.push_back(rows[i] - 1);
-          next_columns.push_back(columns[i]);
-          matrix[next_rows.back()][next_columns.back()] = matrix[rows[i]][columns[i]] + 1;
-          ++visited;
-        }
-        if (rows[i] != matrix.size() - 1 && matrix[rows[i] + 1][columns[i]] == -1) {
-          next_rows.push_back(rows[i] + 1);
-          next_columns.push_back(columns[i]);
-          matrix[next_rows.back()][next_columns.back()] = matrix[rows[i]][columns[i]] + 1;
-          ++visited;
-        }
-        if (columns[i] != 0 && matrix[rows[i]][columns[i - 1]] == -1) {
-          next_rows.push_back(rows[i]);
-          next_columns.push_back(columns[i] - 1);
-          matrix[next_rows.back()][next_columns.back()] = matrix[rows[i]][columns[i]] + 1;
-          ++visited;
-        }
-        if (columns[i] != matrix[0].size() - 1 && matrix[rows[i]][columns[i] + 1] == -1) {
-          next_rows.push_back(rows[i]);
-          next_columns.push_back(columns[i] + 1);
-          matrix[next_rows.back()][next_columns.back()] = matrix[rows[i]][columns[i]] + 1;
-          ++visited;
+    vector<vector<int>> distance_matrix(matrix.size(), vector<int>(matrix[0].size(), -1));
+    vector<std::pair<int, int>> current_level;
+    for (int row = 0; row < matrix.size(); ++row) {
+      for (int column = 0; column < matrix[0].size(); ++column) {
+        if (matrix[row][column] == 0) {
+          current_level.push_back({row, column});
+          distance_matrix[row][column] = 0;
         }
       }
-      rows.swap(next_rows);
-      columns.swap(next_columns);
-      next_rows.clear();
-      next_columns.clear();
     }
-    return matrix;
+    int distance = 1;
+    const int bottom_boundary = matrix.size(), right_boundary = matrix[0].size();
+    do {
+      vector<std::pair<int, int>> next_level;
+      for (const auto& coordinate : current_level) {
+        // up
+        if (0 <= coordinate.first - 1 && distance_matrix[coordinate.first - 1][coordinate.second] == -1) {
+          distance_matrix[coordinate.first - 1][coordinate.second] = distance;
+          next_level.push_back({coordinate.first - 1, coordinate.second});
+        }
+        // down
+        if (coordinate.first + 1 < bottom_boundary && distance_matrix[coordinate.first + 1][coordinate.second] == -1) {
+          distance_matrix[coordinate.first + 1][coordinate.second] = distance;
+          next_level.push_back({coordinate.first + 1, coordinate.second});
+        }
+        // left
+        if (0 <= coordinate.second - 1 && distance_matrix[coordinate.first][coordinate.second - 1] == -1) {
+          distance_matrix[coordinate.first][coordinate.second - 1] = distance;
+          next_level.push_back({coordinate.first, coordinate.second - 1});
+        }
+        // right
+        if (coordinate.second + 1 < right_boundary && distance_matrix[coordinate.first][coordinate.second + 1] == -1) {
+          distance_matrix[coordinate.first][coordinate.second + 1] = distance;
+          next_level.push_back({coordinate.first, coordinate.second + 1});
+        }
+      }
+      ++distance;
+      current_level = next_level;
+    } while (!current_level.empty());
+    return distance_matrix;
   }
 };
 
 int main(void) {
-  vector<vector<int>> test_vec{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+  vector<vector<int>> test_vec{{0, 0, 0}};
   Solution sln;
   auto ret = sln.updateMatrix(test_vec);
   return 0;
