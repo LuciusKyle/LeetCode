d00dc63506ea07f291d6e9a54bc67dfb99e4f015
LuciusKyle <LuciusKyle@outlook.com>
2022-05-16 13:59:08 +0000

    0518 beats 5%.

--- 0518_Coin_Change_2/0518.cc
+++ 0518_Coin_Change_2/0518.cc
@@ -1,25 +1,41 @@
 
 #include <assert.h>
+
+#include <array>
 #include <vector>
 
+using std::array;
 using std::vector;
+
+constexpr int kMaxAmount = 5000;
 
 class Solution {
  public:
-  int change(int amount, const vector<int>& coins) {
-    if (coins.empty()) return 0;
-    if (amount == 0) return 1;
-    vector<int> result(amount + 1, 0);
-    for (size_t i = 0; i < size_t(amount + 1); ++i)
-      for (const int coin : coins)
-        if (i + coin < size_t(amount + 1)) result[i + coin] = result[i] + 1;
-    return result.back();
+  int change(const int amount, const vector<int>& coins) {
+    vector<vector<int>> answer_cache(coins.size(), vector<int>(amount + 1));
+    for (int i = 0; i < answer_cache[0].size(); i += coins[0])
+      answer_cache[0][i] = 1;
+
+    for (size_t i = 1; i < answer_cache.size(); ++i) {
+      for (int j = 0; j < amount + 1; ++j) {
+        if (answer_cache[i - 1][j]) {
+          for (int k = j; k < amount + 1; k += coins[i]) {
+            answer_cache[i][k] += answer_cache[i - 1][j];
+          }
+        }
+      }
+    }
+    return answer_cache[coins.size() - 1][amount];
   }
+
+ private:
 };
 
-int main(void ) {
+int main(void) {
   Solution sln;
-  assert(4 == sln.change(5, {1, 2, 5}));
-  assert(0 == sln.change(3, {2}));
+  int rtn = sln.change(5, {1, 2, 5});
+  assert(4 == rtn);
+  rtn = sln.change(3, {2});
+  assert(0 == rtn);
   return 0;
 }
