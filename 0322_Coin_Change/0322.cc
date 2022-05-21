74a5512ac0e0c1e703f056dbd78fb73bca39fce1
LuciusKyle <LuciusKyle@outlook.com>
2022-05-21 12:08:02 +0000

    0322 beats 90%.

--- 0322_Coin_Change/0322.cc
+++ 0322_Coin_Change/0322.cc
@@ -1,52 +1,58 @@
 
 #include <assert.h>
 #include <limits.h>
+
 #include <algorithm>
 #include <vector>
 
 using std::vector;
 
+// DFS, beats 88%.
+class Solution_BFS {
+ public:
+  int coinChange(const vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, -1);
+    dp[0] = 0;
+    vector<int> next_level(1, 0);
+    int level = 1;
+    do {
+      vector<int> current_level;
+      for (const int pos : next_level)
+        for (const int coin : coins)
+          if (pos + coin < amount + 1 && dp[pos + coin] == -1) {
+            dp[pos + coin] = level;
+            if (pos + coin == amount) return level;
+            current_level.push_back(pos + coin);
+          }
+      ++level;
+      next_level = current_level;
+    } while (!next_level.empty());
+    return dp.back();
+  }
+};
+
+// beats 90%.
 class Solution {
  public:
   int coinChange(const vector<int>& coins, int amount) {
-    if (coins.empty()) return -1;
-    vector<int> dp(1, 0);
+    vector<int> dp(amount + 1, -1);
+    dp[0] = 0;
     for (int i = 1; i < amount + 1; ++i) {
-      dp.push_back(-1);
       int min_coin_count = INT_MAX;
-      for (const int coin:coins) {
-        if (i - coin < 0 || dp[i - coin] == -1) continue;
-        if (dp[i - coin] < min_coin_count) min_coin_count = dp[i - coin];
-      }
-      if (INT_MAX != min_coin_count) dp.back() = min_coin_count + 1;
+      for (const int coin : coins)
+        if (coin <= coin && dp[i - coin] != -1 && dp[i - coin] < min_coin_count)
+          min_coin_count = dp[i - coin];
+
+      if (INT_MAX != min_coin_count) dp[i] = min_coin_count + 1;
     }
     return dp.back();
-  }
-
- private:
-
-  // not used in this case.
-  int LeastCommonMultiple(const vector<int>& coins, int max_coin) {
-    if (coins.empty()) return 0;
-    int multiple_time = 1;
-    bool keep_running = true;
-    while (keep_running) {
-      keep_running = false;
-      for (const int coin : coins)
-        if ((max_coin * multiple_time) % coin != 0) {
-          ++multiple_time;
-          keep_running = true;
-          break;
-        }
-    }
-    return max_coin * multiple_time;
   }
 };
 
 int main(void) {
   Solution sln;
-  // int rtn = sln.coinChange({186, 419, 83, 408}, 6249);
+  int rtn = sln.coinChange({186, 419, 83, 408}, 6249);
   assert(3 == sln.coinChange({1, 2, 5}, 11));
-  int rtn = sln.coinChange({2}, 3);
+  rtn = sln.coinChange({2}, 3);
   return 0;
 }
