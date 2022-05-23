2acd3013591597d2f62914281fb653a6e915182b
LuciusKyle <LuciusKyle@outlook.com>
2022-05-23 08:47:10 +0000

    0416 only beats 5%?!

--- 0416_Partition_Equal_Subset_Sum/0416.cc
+++ 0416_Partition_Equal_Subset_Sum/0416.cc
@@ -1,39 +1,65 @@
 
-#include <vector>
 #include <map>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
 
-using std::vector;
 using std::map;
+using std::unordered_map;
+using std::vector;
 
 class Solution {
  public:
   bool canPartition(const vector<int>& nums) {
-    vector<map<int, bool>> result(nums.size());
-    int target = 0;
-    int odd_count = 0;
-    for (const int num : nums) {
-      target += num / 2;
-      if (num % 2) ++odd_count;      
-    }
-    if (odd_count % 2 == 1 || nums.size() < 2) return false;
-    target += odd_count / 2;
-    return canPartition(nums, 0, target, result);
+    const int sum = std::accumulate(nums.cbegin(), nums.cend(), 0);
+    const int target = sum / 2;
+    if (target * 2 != sum) return false;
+
+    answer_cache_.assign(nums.size(), unordered_map<int, bool>());
+    // vector<unordered_map<int, bool>>answer_cache(nums.size());
+    return canPartition(nums, 0, target);
   }
 
  private:
-  bool canPartition(const vector<int>& nums, const size_t cur_index, const int target, vector<map<int, bool>>& result) {
-    // if (nums.empty()) return false; // don't check empty at here.
-    if (nums.size() - cur_index == 0) return false;
-    if (nums.size() - cur_index == 1) return nums[cur_index] == target;
-    if (result[cur_index].find(target) != result[cur_index].end()) return result[cur_index].at(target);
-    const bool rtn = canPartition(nums, cur_index + 1, target, result) || canPartition(nums, cur_index + 1, target - nums[cur_index], result);
-    result[cur_index].insert({target, rtn});
-    return rtn;
+  bool canPartition(const vector<int>& nums, const size_t cur_index, const int target) {
+    if (answer_cache_[cur_index].count(target)) return answer_cache_[cur_index].at(target);
+    if (cur_index == nums.size() - 1)
+      return target == 0 || target == nums.back();
+    if (target == 0)
+      return true;
+    if (target < 0) {
+      answer_cache_[cur_index].insert({target, false});
+      return false;
+    }
+    bool result = canPartition(nums, cur_index + 1, target) || canPartition(nums, cur_index + 1, target - nums[cur_index]);
+    answer_cache_[cur_index].insert({target, result});
+    return result;
   }
+  
+  bool canPartition(const vector<int>& nums, const size_t cur_index, const int target, vector<unordered_map<int, bool>>& answer_cache) {
+    if (answer_cache[cur_index].count(target)) return answer_cache[cur_index].at(target);
+    if (cur_index == nums.size() - 1)
+      return target == 0 || target == nums.back();
+    if (target == 0)
+      return true;
+    if (target < 0) {
+      answer_cache[cur_index].insert({target, false});
+      return false;
+    }
+    bool result = canPartition(nums, cur_index + 1, target, answer_cache) || canPartition(nums, cur_index + 1, target - nums[cur_index], answer_cache);
+    answer_cache[cur_index].insert({target, result});
+    return result;
+  }
+
+  vector<unordered_map<int, bool>> answer_cache_;
 };
 
 int main(void) {
+  vector<int> test_vec{100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
   Solution sln;
+  bool result = sln.canPartition(test_vec);
+  test_vec = {2, 2, 3, 5};
+  result = sln.canPartition(test_vec);
   return 0;
 }
 
