1a00cbf453e056e6c7979159f2dd432a446a2791
LuciusKyle <LuciusKyle@outlook.com>
2022-05-24 06:47:59 +0000

    0041 beats 75%.

--- 0041_First_Missing_Positive/0041.cc
+++ 0041_First_Missing_Positive/0041.cc
@@ -1,6 +1,7 @@
 
 #include <assert.h>
 #include <limits.h>
+
 #include <set>
 #include <unordered_set>
 #include <vector>
@@ -9,40 +10,15 @@ using std::set;
 using std::unordered_set;
 using std::vector;
 
-class Solution_V1 {
- public:
-  int firstMissingPositive(vector<int>& nums) {
-    set<int> result;
-    for (const int num : nums)
-      if (num > 0) result.insert(num);
-
-    int pre = 0;
-    for (const int num : result)
-      if (num - pre > 1)
-        return pre + 1;
-      else
-        pre = num;
-    if (result.empty()) return 1;
-    return *(result.cend()) + 1;
-  }
-};
-
 class Solution {
  public:
   int firstMissingPositive(vector<int>& nums) {
-    unordered_set<int> result;
-    result.reserve(nums.size());
-    int min_positive = -1;
+    vector<int> dict(nums.size(), 1);
     for (const int num : nums)
-      if (num > 0) {
-        if (min_positive < 0 || num < min_positive) min_positive = num;
-        result.insert(num);
-      }
-    if (min_positive == -1 || min_positive > 1) return 1;
-    for (int i = min_positive; i < INT_MAX; ++i) {
-      if (result.count(i) == 0) return i;
-    }
-    return 0;
+      if (0 < num && num < nums.size() + 1) dict[num - 1] = 0;
+    for (int i = 0; i < nums.size(); ++i)
+      if (dict[i]) return i + 1;
+    return nums.size() + 1;
   }
 };
 
