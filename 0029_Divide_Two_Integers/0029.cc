8b3211f23f03379a6f2033d59fc373fae84ee97d
LuciusKyle <LuciusKyle@outlook.com>
2022-05-30 05:55:07 +0000

    0029 solved.

--- 0029_Divide_Two_Integers/0029.cc
+++ 0029_Divide_Two_Integers/0029.cc
@@ -1,25 +1,24 @@
 
-#include<cmath>
+#include <limits.h>
+#include <math.h>
 
-// this is not my solution. code was copied from LeetCode Discuss.
-// https://leetcode.com/problems/divide-two-integers/discuss/13407/Detailed-Explained-8ms-C++-solution
 class Solution {
  public:
   int divide(int dividend, int divisor) {
-    if (!divisor || (dividend == INT_MIN && divisor == -1)) return INT_MAX;
-    int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-    long long dvd = labs(dividend);
-    long long dvs = labs(divisor);
-    int res = 0;
-    while (dvd >= dvs) {
-      long long temp = dvs, multiple = 1;
-      while (dvd >= (temp << 1)) {
-        temp <<= 1;
-        multiple <<= 1;
-      }
-      dvd -= temp;
-      res += multiple;
+    if (dividend == INT_MIN && divisor == -1) {
+      return INT_MAX;
     }
-    return sign == 1 ? res : -res;
+    int ans = dividend / divisor;
+    if (ans >= INT_MAX)
+      return INT_MAX;
+    if (ans <= INT_MIN)
+      return INT_MIN;
+    return floor(ans);
   }
 };
+
+int main(void) {
+  Solution sln;
+  int result = sln.divide(-2147483648, -1);
+  return 0;
+}
