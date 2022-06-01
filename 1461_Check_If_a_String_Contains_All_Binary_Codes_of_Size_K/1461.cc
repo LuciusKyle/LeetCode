5d6434660e67bff2102074fc33e1282315624aac
LuciusKyle <LuciusKyle@outlook.com>
2022-06-01 02:51:19 +0000

    1461 solved.

--- 1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K/1461.cc
+++ 1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K/1461.cc
@@ -11,7 +11,7 @@ using std::string;
 class Solution {
  public:
   bool hasAllCodes(string s, int k) {
-    if (s.size() < k) return false;
+    if (s.size() + 1 < (1 << k) + k) return false;
     std::unordered_set<string> result_set;
     result_set.reserve(1 << k);
     for (size_t i = 0; i <= s.size() - k; ++i)
