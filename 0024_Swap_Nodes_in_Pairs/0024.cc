6470240341722d1274096479f8139fbc40077b88
LuciusKyle <LuciusKyle@outlook.com>
2022-06-10 14:03:44 +0000

    0024 and 0025 solved.

--- 0024_Swap_Nodes_in_Pairs/0024.cc
+++ 0024_Swap_Nodes_in_Pairs/0024.cc
@@ -1,16 +1,43 @@
 
+#include <stdlib.h>
+#include <vector>
 
+// Definition for singly-linked list.
 struct ListNode {
   int val;
   ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
  public:
   ListNode* swapPairs(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+    ListNode *cur_node = head, *next_node = head == nullptr ? nullptr : head->next;
+    // use recursion.
+    // if no recursion, then 4 pointers are needed.
+    next_node->next = swapPairs(next_node->next);
 
-
-    return head;
+    cur_node->next = next_node->next;
+    next_node->next = cur_node;
+    return next_node;
   }
 };
+
+int main(int argc, char * argv[] ) {
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+
+  Solution sln;
+  ListNode * rtn_head = sln.swapPairs(head);
+  while (rtn_head) {
+    ListNode *temp_node = rtn_head->next;
+    delete rtn_head;
+    rtn_head = temp_node;
+  }
+  return 0;
+}
--- 0025_Reverse_Nodes_in_k-Group/0025.cc
+++ 0025_Reverse_Nodes_in_k-Group/0025.cc
@@ -1,36 +1,52 @@
 
-#include<iostream>
-
+// Definition for singly-linked list.
 struct ListNode {
   int val;
-  ListNode* next;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
  public:
-  ListNode* reverseKGroup(ListNode* head, int k) {}
-
- public:
-  ListNode *reverse(ListNode *node, const int count, const int k){
-    if(count == k){
-      return node;
+  ListNode *reverseKGroup(ListNode *head, const int k) {
+    ListNode ret_head(0);
+    ret_head.next = head;
+    ListNode *pre_tail = &ret_head, *cur_head = head, *cur_ptr = head;
+    int node_count = 1;
+    while (cur_ptr) {
+      if (node_count == k) {
+        ListNode *pre_node = pre_tail, *cur_node = cur_head, *next_node = cur_node->next;
+        while (cur_node != cur_ptr) {
+          cur_node->next = pre_node;
+          pre_node = cur_node;
+          cur_node = next_node;
+          next_node = next_node->next;
+        }
+        cur_ptr->next = pre_node;
+        pre_tail->next = cur_node;
+        cur_head->next = next_node;
+        cur_ptr = cur_head;
+        pre_tail = cur_head;
+        cur_head = next_node;
+        node_count = 0;
+      }
+      cur_ptr = cur_ptr->next;
+      ++node_count;
     }
-    node = reverse(node->next, count + 1, k);
-    return node;
+    return ret_head.next;
   }
 };
 
-int main(void)
-{
+int main(void) {
+  ListNode node1(1), node2(2), node3(3), node4(4), node5(5);
+  node1.next = &node2;
+  node2.next = &node3;
+  node3.next = &node4;
+  node4.next = &node5;
   Solution sln;
-  ListNode *head = new ListNode(1);
-  ListNode *working_ptr = head;
-  for (size_t i = 1; i < 5; ++i) {
-    working_ptr->next = new ListNode(i + 1);
-    working_ptr = working_ptr->next;
-  }
-  ListNode *rtn = sln.reverse(head, 1, 5);
+  sln.reverseKGroup(&node1, 2);
 
   return 0;
 }
