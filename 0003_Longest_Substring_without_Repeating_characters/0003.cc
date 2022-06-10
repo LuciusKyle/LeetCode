deb11f4b5775e868613543e41dd5ffddcbd4e24b
LuciusKyle <LuciusKyle@outlook.com>
2022-06-10 06:57:30 +0000

    0003 solved.

--- 0003_Longest_Substring_without_Repeating_characters/0003.cc
+++ 0003_Longest_Substring_without_Repeating_characters/0003.cc
@@ -9,15 +9,14 @@ using std::string;
 class Solution {
  public:
   int lengthOfLongestSubstring(const string s) {
-    std::array<size_t, 0x100> dict{0};
-    size_t max_len = 0;
-    size_t start = 0;
-    for (size_t i = 1; i <= s.size(); ++i) {
+    int dict[128] = {0};
+    int max_len = 0;
+    for (int i = 1, start = 0; i <= s.size(); ++i) {
       if (start < dict[s[i - 1]]) start = dict[s[i - 1]];
       dict[s[i - 1]] = i;
       if (max_len < i - start) max_len = i - start;
     }
-    return static_cast<int>(max_len);
+    return max_len;
   }
 };
 
