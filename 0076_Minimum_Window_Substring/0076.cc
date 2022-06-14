faec4d8d3ebb1220defe1dedcaa4c277c0ae0d48
LuciusKyle <LuciusKyle@outlook.com>
2022-06-14 16:02:22 +0000

    0076 solved.

--- 0076_Minimum_Window_Substring/0076.cc
+++ 0076_Minimum_Window_Substring/0076.cc
@@ -1,5 +1,6 @@
 
 #include <assert.h>
+
 #include <array>
 #include <iostream>
 #include <string>
@@ -12,71 +13,59 @@ using std::string;
 class Solution {
  public:
   string minWindow(const string s, const string t) {
-    static_assert(sizeof(size_t) == 8);
+    int s_count[58] = {'\0'}, t_count[58] = {'\0'};
+    for (const char ch : t) ++t_count[ch - 'A'];
 
-    size_t ch_dict[4] = {0};
-    array<bool, 256> ch_dict_arr{false};
-    array<size_t, 256> ch_count{0};
-    for (const auto ch : t) {
-      ch_dict[ch / 64] |= (1 << (ch % 64));
-      ch_dict_arr[ch] = true;
-    }
-    size_t max_len = s.size();
-    size_t start_index = 0;
-    size_t end_index = 0;
-    size_t ch_result[4] = {0};
-    for (size_t i = 0; i < s.size(); ++i) {
-      if (max_len < i - start_index) {
-        const size_t temp_start_index = i < max_len ? 0 : i - max_len;
-        if (ch_dict_arr[s[temp_start_index]]) {
-          assert(ch_count[s[temp_start_index]] != 0);
-          if (ch_count[s[temp_start_index]] == 1)
-            ch_result[s[temp_start_index] / 64] &=
-                (1 << (s[temp_start_index] % 64));
-          --ch_count[s[temp_start_index]];
-        }
-      }
-      if (!ch_dict_arr[s[i]]) continue;
-      ++ch_count[s[i]];
-      if (ch_count[s[i]] == 1) {
-        ch_result[s[i] / 64] |= (1 << (s[i] % 64));
-      }
-      if (sameArr(ch_dict, ch_result)) {
-        // shrink window.
-        end_index = i + 1;
-        max_len = i - start_index;
-        for (start_index = i < max_len ? 0 : i - max_len; start_index < i; ++start_index) {
-          if (i - start_index < max_len) max_len = i - start_index;
-          if (!ch_dict_arr[s[start_index]]) continue;
-          assert(ch_count[s[start_index]] != 0);
-          if (ch_count[s[start_index]] == 1)
-            break;
-          else
-            --ch_count[s[start_index]];
-        }
+    size_t l_index = 0, r_index = 0;
+    // enlarge window
+    while (r_index < s.size() && !includeAllCharacter(s_count, t_count))
+      ++s_count[s[r_index++] - 'A'];
+
+    // is there one possible answer?
+    if (!includeAllCharacter(s_count, t_count)) return "";
+
+    size_t temp_l_index = l_index, temp_r_index = r_index;
+    --s_count[s[--r_index] - 'A'];
+    // move window
+    while (r_index < s.size()) {
+      ++s_count[s[r_index++] - 'A'];
+      // try to shrink window
+      if (includeAllCharacter(s_count, t_count))
+        do {
+          --s_count[s[l_index++] - 'A'];
+        } while (includeAllCharacter(s_count, t_count));
+      else
+        --s_count[s[l_index++] - 'A'];
+
+      if (r_index - l_index + 1 != temp_r_index - temp_l_index) {
+        temp_r_index = r_index;
+        temp_l_index = l_index - 1;
       }
     }
-
-    if (max_len == s.size())
-      return "";
-    else
-      return {s.cbegin() + start_index, s.cbegin() + end_index};
+    if (r_index - l_index + 1 != temp_r_index - temp_l_index) {
+      temp_r_index = r_index;
+      temp_l_index = l_index - 1;
+    }
+    return s.substr(temp_l_index, temp_r_index - temp_l_index);
   }
 
  private:
-  bool sameArr(const size_t *a, const size_t *b, const size_t len = 4) const {
-    for (size_t i = 0; i < len; ++i)
-      if (a[i] != b[i]) return false;
-
+  bool includeAllCharacter(const int *s_count, const int *t_count) {
+    for (int i = 0; i < 58; ++i)
+      if (0 < t_count[i] - s_count[i]) return false;
     return true;
   }
 };
 
 int main(void) {
   Solution sln;
-  assert("BANC" == sln.minWindow("ADOBECODEBANC", "ABC"));
-
-  auto rtn = sln.minWindow("wegdtzwabazduwwdysdetrrctotpcepalxdewzezbfewbabbseinxbqqplitpxtcwwhuyntbtzxwzyaufihclztckdwccpeyonumbpnuonsnnsjscrvpsqsftohvfnvtbphcgxyumqjzltspmphefzjypsvugqqjhzlnylhkdqmolggxvneaopadivzqnpzurmhpxqcaiqruwztroxtcnvhxqgndyozpcigzykbiaucyvwrjvknifufxducbkbsmlanllpunlyohwfsssiazeixhebipfcdqdrcqiwftutcrbxjthlulvttcvdtaiwqlnsdvqkrngvghupcbcwnaqiclnvnvtfihylcqwvderjllannflchdklqxidvbjdijrnbpkftbqgpttcagghkqucpcgmfrqqajdbynitrbzgwukyaqhmibpzfxmkoeaqnftnvegohfudbgbbyiqglhhqevcszdkokdbhjjvqqrvrxyvvgldtuljygmsircydhalrlgjeyfvxdstmfyhzjrxsfpcytabdcmwqvhuvmpssingpmnpvgmpletjzunewbamwiirwymqizwxlmojsbaehupiocnmenbcxjwujimthjtvvhenkettylcoppdveeycpuybekulvpgqzmgjrbdrmficwlxarxegrejvrejmvrfuenexojqdqyfmjeoacvjvzsrqycfuvmozzuypfpsvnzjxeazgvibubunzyuvugmvhguyojrlysvxwxxesfioiebidxdzfpumyon", "ozgzyywxvtublcl");
-
+  auto rtn = sln.minWindow("ADOBECODEBANCabc", "ABC");
+  assert("BANC" == rtn);
+  rtn = sln.minWindow("ADOBECODEBANC", "ABC");
+  assert("BANC" == rtn);
+  rtn = sln.minWindow("a", "a");
+  assert("a" == rtn);
+  rtn = sln.minWindow("wegdtzwabazduwwdysdetrrctotpcepalxdewzezbfewbabbseinxbqqplitpxtcwwhuyntbtzxwzyaufihclztckdwccpeyonumbpnuonsnnsjscrvpsqsftohvfnvtbphcgxyumqjzltspmphefzjypsvugqqjhzlnylhkdqmolggxvneaopadivzqnpzurmhpxqcaiqruwztroxtcnvhxqgndyozpcigzykbiaucyvwrjvknifufxducbkbsmlanllpunlyohwfsssiazeixhebipfcdqdrcqiwftutcrbxjthlulvttcvdtaiwqlnsdvqkrngvghupcbcwnaqiclnvnvtfihylcqwvderjllannflchdklqxidvbjdijrnbpkftbqgpttcagghkqucpcgmfrqqajdbynitrbzgwukyaqhmibpzfxmkoeaqnftnvegohfudbgbbyiqglhhqevcszdkokdbhjjvqqrvrxyvvgldtuljygmsircydhalrlgjeyfvxdstmfyhzjrxsfpcytabdcmwqvhuvmpssingpmnpvgmpletjzunewbamwiirwymqizwxlmojsbaehupiocnmenbcxjwujimthjtvvhenkettylcoppdveeycpuybekulvpgqzmgjrbdrmficwlxarxegrejvrejmvrfuenexojqdqyfmjeoacvjvzsrqycfuvmozzuypfpsvnzjxeazgvibubunzyuvugmvhguyojrlysvxwxxesfioiebidxdzfpumyon", "ozgzyywxvtublcl");
+  assert("tcnvhxqgndyozpcigzykbiaucyvwrjvknifufxducbkbsmlanl" == rtn);
   return 0;
 }
