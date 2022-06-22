c0fd4e8a219ccb42a5da748c7892be79c3dbb358
LuciusKyle <LuciusKyle@outlook.com>
2022-06-22 01:17:43 +0000

    0215 add testcase. solution not changed.

--- 0215_Kth_Largest_Element_in_an_Array/0215.cc
+++ 0215_Kth_Largest_Element_in_an_Array/0215.cc
@@ -1,6 +1,12 @@
 
+#include <assert.h>
 #include <limits.h>
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
 #include <set>
+#include <thread>
 #include <vector>
 
 using std::multiset;
@@ -21,8 +27,23 @@ class Solution {
 
 int main(void) {
   Solution sln;
-  int rtn = sln.findKthLargest({3, 2, 3, 1, 2, 4, 5, 5, 6}, 4);
-  rtn = sln.findKthLargest({3, 2, 1, 5, 6, 4}, 2);
+
+  auto start = std::chrono::system_clock::now();
+  vector<int> test_vec;
+  FILE* fp = fopen("testcase1.txt", "rb");
+  char* buf = new char[2 * 1024 * 1024];
+  const size_t read_size = fread(buf, 1, 2 * 1024 * 1024, fp);
+  assert(read_size < 2 * 1024 * 1024);
+  for (size_t i = 1; i <= read_size; ++i)
+    if ((!('0' <= buf[i - 1] && buf[i - 1] <= '9')) && ('0' <= buf[i] && buf[i] <= '9')) test_vec.push_back(atoi(buf + i));
+  fclose(fp);
+  delete[] buf;
+  const int test_k = test_vec.back();
+  test_vec.pop_back();
+  std::cout << "time to prepare data: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << " milliseconds" << std::endl;
+  start = std::chrono::system_clock::now();
+  int rtn = sln.findKthLargest(test_vec, test_k);
+  std::cout << "time to calculate: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << " milliseconds" << std::endl;
 
   return 0;
 }
