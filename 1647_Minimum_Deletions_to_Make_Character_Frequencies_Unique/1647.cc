8699544b8f59c08da0445d6cf1d5bffbeb448ade
LuciusKyle <LuciusKyle@outlook.com>
2022-06-28 01:02:05 +0000

    1647 solved.

---
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique/1647.cc
@@ -0,0 +1,37 @@
+
+#include <assert.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+ public:
+  int minDeletions(const string s) {
+    int freq[26] = {0};
+    for (const char ch : s) ++freq[ch - 'a'];
+    std::sort(std::begin(freq), std::end(freq));
+    int answer = 0;
+    for (int i = 24; 0 <= i; --i)
+      if (freq[i + 1] <= freq[i])
+        if (freq[i + 1] == 0) {
+          answer += freq[i];
+          freq[i] = 0;
+        } else {
+          answer += freq[i] - (freq[i + 1] - 1);
+          freq[i] = freq[i + 1] - 1;
+        }
+
+    return answer;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  Solution sln;
+  const string test_str = argc < 2 ? "aab" : argv[1];
+  int ret = sln.minDeletions(test_str);
+  std::cout << ret << std::endl;
+  return 0;
+}
