f34f1f9f28951d2b53802cc2a20917ca7d4046b5
LuciusKyle <LuciusKyle@outlook.com>
2022-06-29 01:55:02 +0000

    0016 beats 22%.

--- 0016_3Sum_Closest/0016.cc
+++ 0016_3Sum_Closest/0016.cc
@@ -1,45 +1,32 @@
 
-#include <cmath>
-#include <queue>
+#include <math.h>
+
 #include <vector>
 
-using std::priority_queue;
 using std::vector;
 
 class Solution {
  public:
-  int threeSumClosest(vector<int>& nums, int target) {
-    int i, j, k, diff, sum, minSum;
-    int min = 99999999;
-    int len = nums.size();
-
-    priority_queue<int> q;
-    for (i = 0; i < len; i++) q.push(nums[i]);
-
-    vector<int> a;
-    for (i = 0; i < len; i++) {
-      a.push_back(q.top());
-      q.pop();
-    }  // now a is in descending order
-
-    for (i = 0; i < len; i++) {
-      j = i + 1;
-      k = len - 1;
-      while (j < k) {
-        sum = a[i] + a[j] + a[k];
-        diff = sum - target;
-        if (diff == 0)
-          return sum;
-        else if (diff > 0)
-          j++;
-        else
-          k--;
-        if (abs(diff) < min) {
-          minSum = sum;
-          min = abs(diff);
+  int threeSumClosest(vector<int>& nums, const int target) {
+    std::sort(nums.begin(), nums.end());
+    int closest_sum = nums[0] + nums[1] + nums[2];
+    int closest_distance = abs(closest_sum - target);
+    for (int i = 0; i + 2 < nums.size(); ++i) {
+      int l_index = i + 1, r_index = nums.size() - 1;
+      while (l_index < r_index) {
+        const int temp_sum = nums[i] + nums[l_index] + nums[r_index];
+        if (abs(temp_sum - target) < closest_distance) {
+          closest_distance = abs(temp_sum - target);
+          closest_sum = temp_sum;
         }
+        if (temp_sum < target)
+          ++l_index;
+        else if (target < temp_sum)
+          --r_index;
+        else
+          return target;
       }
     }
-    return minSum;
+    return closest_sum;
   }
 };
