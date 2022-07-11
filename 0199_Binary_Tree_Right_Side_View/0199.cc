2f16db9824db68b70181f68e9bd92d9b8446d482
LuciusKyle <LuciusKyle@outlook.com>
2022-07-11 01:52:36 +0000

    0199 solved.

--- 0199_Binary_Tree_Right_Side_View/0199.cc
+++ 0199_Binary_Tree_Right_Side_View/0199.cc
@@ -1,14 +1,18 @@
 
+#include <stack>
 #include <vector>
 
 using std::vector;
+using std::stack;
 
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
   TreeNode *left;
   TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
@@ -16,18 +20,17 @@ class Solution {
   vector<int> rightSideView(TreeNode *root) {
     if (root == nullptr) return vector<int>();
     vector<int> rtn;
-    preorderTraversalFromRight(root, 0, rtn);
-    vector<int> rtn2;
-    rtn2.push_back(root->val);
-    for (const int val : rtn) rtn2.push_back(val);
-    return rtn2;
-  }
 
- private:
-  void preorderTraversalFromRight(TreeNode *node, const int cur_depth, vector<int> &rtn) {
-    if (node == nullptr) return;
-    if (int(rtn.size()) < cur_depth) rtn.push_back(node->val);
-    preorderTraversalFromRight(node->right, cur_depth + 1, rtn);
-    preorderTraversalFromRight(node->left, cur_depth + 1, rtn);
+    stack<std::pair<TreeNode *, int>> stk;
+    stk.push({root, 1});
+    do {
+      auto node_depth_pari = stk.top();
+      if (rtn.size() < node_depth_pari.second)
+        rtn.push_back(node_depth_pari.first->val);
+      stk.pop();
+      if (node_depth_pari.first->left) stk.push({node_depth_pari.first->left, node_depth_pari.second + 1});
+      if (node_depth_pari.first->right) stk.push({node_depth_pari.first->right, node_depth_pari.second + 1});
+    } while (!stk.empty());
+    return rtn;
   }
 };
