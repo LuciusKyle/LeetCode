d9ff9729fc50207349e2cc4eb48cc283f4de9653
LuciusKyle <LuciusKyle@outlook.com>
2022-07-22 12:46:46 +0000

    2161 solved.

--- 2161_Partition_Array_According_to_Given_Pivot/2161.cc
+++ 2161_Partition_Array_According_to_Given_Pivot/2161.cc
@@ -11,28 +11,21 @@ class Solution {
  public:
   vector<int> pivotArray(vector<int>& nums, const int pivot) {
     vector<int> greater_nums;
-    if (nums.size() < 256)
-      greater_nums.reserve(nums.size());
-    else if (nums.size() < 1024)
-      greater_nums.reserve(256);
-    else if (nums.size() < 5000)
-      greater_nums.reserve(1024);
-    else
-      greater_nums.reserve(5000);
+    greater_nums.reserve(nums.size() < 256 ? nums.size() : 256);
     int equal_count = 0, cur_index = 0;
-    for (int i = 0; i < nums.size(); ++i) {
+    for (int i = 0; i < nums.size(); ++i)
       if (nums[i] < pivot)
         nums[cur_index++] = nums[i];
       else if (pivot < nums[i])
         greater_nums.push_back(nums[i]);
       else
         ++equal_count;
-    }
     if (cur_index == nums.size()) return nums;
 
     for (const int cur_index_pos = cur_index; cur_index < cur_index_pos + equal_count; ++cur_index)
       nums[cur_index] = pivot;
-    memcpy(nums.data() + cur_index, greater_nums.data(), greater_nums.size() * sizeof(int));
+    if (!greater_nums.empty())
+      memcpy(nums.data() + cur_index, greater_nums.data(), greater_nums.size() * sizeof(int));
     return nums;
   }
 };
