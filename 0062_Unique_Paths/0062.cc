7bbbf039caf17d8a71ae50e535c9653b19cb5467
LuciusKyle <LuciusKyle@outlook.com>
2022-08-01 00:10:19 +0000

    0062 solved.

--- 0062_Unique_Paths/0062.cc
+++ 0062_Unique_Paths/0062.cc
@@ -17,8 +17,7 @@ class Solution {
   int uniquePaths(vector<vector<int>> &dp, const int row, const int column) {
     if (dp[row][column] != INT_MAX) return dp[row][column];
     if (row == 0 || column == 0) return 1;
-    return dp[row][column] = uniquePaths(dp, row - 1, column) +
-                             uniquePaths(dp, row, column - 1);
+    return dp[row][column] = uniquePaths(dp, row - 1, column) + uniquePaths(dp, row, column - 1);
   }
 };
 
@@ -27,11 +26,6 @@ int main(void) {
   auto start = std::chrono::system_clock::now();
   int rtn = sln.uniquePaths(3, 7);
   // do something.
-  std::cout << "time: "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(
-                   std::chrono::system_clock::now() - start)
-                   .count()
-            << " milliseconds" << std::endl;
-
+  std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() << " milliseconds" << std::endl;
   return 0;
 }
