ee69a9f238637e2b21e3503de7452ef8661a3723
LuciusKyle <LuciusKyle@outlook.com>
2022-08-18 01:00:13 +0000

    1338 solved.

--- 1338_Reduce_Array_Size_to_The_Half/1338.cc
+++ 1338_Reduce_Array_Size_to_The_Half/1338.cc
@@ -11,11 +11,16 @@ using std::vector;
 class Solution {
  public:
   int minSetSize(vector<int>& arr) {
-    vector<int> nums_count(100001, 0);
+    // vector<int> nums_count(100001, 0); // stack or heap? pick one!
+    int nums_count[100001] = {0};  // stack or heap? pick one!
     for (const int num : arr) ++nums_count[num];
-    std::sort(nums_count.rbegin(), nums_count.rend());
-    for (int i = 0, count = 0, target_size = arr.size() / 2; i < 100000; ++i) {
-      count += nums_count[i];
+    vector<int> active_nums_count;
+    active_nums_count.reserve(arr.size());
+    for (const int num : nums_count)
+      if (num) active_nums_count.push_back(num);
+    std::sort(active_nums_count.rbegin(), active_nums_count.rend());
+    for (int i = 0, count = 0, target_size = arr.size() / 2; i < active_nums_count.size(); ++i) {
+      count += active_nums_count[i];
       if (target_size <= count) return i + 1;
     }
     return arr.size();
