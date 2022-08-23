65d0f1e998d65860b2918476cbab20eefe33a8a3
LuciusKyle <LuciusKyle@outlook.com>
2022-08-23 04:12:37 +0000

    0234 solved.

--- 0234_Palindrome_Linked_List/0234.cc
+++ 0234_Palindrome_Linked_List/0234.cc
@@ -4,22 +4,39 @@
 // Definition for singly-linked list.
 struct ListNode {
   int val;
-  ListNode* next;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-// it's impossable to solve this problem using constant space and without modifying the list.
+// it's impossable to solve this problem using constant space without modifying the list.
 class Solution {
  public:
-  bool isPalindrome(ListNode* head) {
-    ListNode* temp_ptr = head;
-    std::vector<int> node_vec;
+  bool isPalindrome(ListNode *head) {
+    int node_count = 0;
+    ListNode *temp_ptr = head;
     while (temp_ptr != nullptr) {
-      node_vec.push_back(temp_ptr->val);
+      ++node_count;
       temp_ptr = temp_ptr->next;
     }
-    for (size_t i = 0; i < node_vec.size() / 2; ++i)
-      if (node_vec[i] != node_vec[node_vec.size() - i - 1]) return false;
+    if (node_count == 1) return true;
+    temp_ptr = nullptr;
+    ListNode *next_ptr = head;
+    for (int i = 0; i < node_count / 2; ++i) {
+      ListNode *pre_ptr = temp_ptr;
+      temp_ptr = next_ptr;
+      next_ptr = next_ptr->next;
+      temp_ptr->next = pre_ptr;
+    }
+    ListNode auxiliary_node(node_count % 2 ? next_ptr->val : temp_ptr->val);
+    auxiliary_node.next = node_count % 2 ? temp_ptr : temp_ptr->next;
+    temp_ptr = &auxiliary_node;
+    while (temp_ptr != nullptr) {
+      if (temp_ptr->val != next_ptr->val) return false;
+      temp_ptr = temp_ptr->next;
+      next_ptr = next_ptr->next;
+    }
     return true;
   }
 };
