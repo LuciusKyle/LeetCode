edc664598b924662d8ea40c2a3a58b0a6284361d
LuciusKyle <LuciusKyle@outlook.com>
2022-08-25 01:40:44 +0000

    0383 solved.

--- 0383_Ransom_Note/0383.cc
+++ 0383_Ransom_Note/0383.cc
@@ -8,13 +8,13 @@ using std::string;
 class Solution {
  public:
   bool canConstruct(const string ransomNote, const string magazine) {
-    array<size_t, 26> letters_count{0};
-    for (const char ch : magazine) ++letters_count[ch - 'a'];
-    for (const char ch : ransomNote)
-      if (letters_count[ch - 'a'] == 0)
-        return false;
-      else
-        --letters_count[ch - 'a'];
+    int magazine_count[26] = {0};
+    for (const char ch : magazine) ++magazine_count[ch - 'a'];
+    int ransomNote_count[26] = {0};
+    for (const char ch : ransomNote) ++ransomNote_count[ch - 'a'];
+    for (int i = 0; i < std::end(ransomNote_count) - std::begin(ransomNote_count); ++i)
+      if (ransomNote_count[i] != 0 && ransomNote_count[i] < magazine_count[i]) return false;
+
     return true;
   }
 };
