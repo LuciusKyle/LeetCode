8525cdeb96f103c6b41d1daad3277954215510aa
Lucius Kyle <luciuskyle@outlook.com>
2022-09-07 02:28:35 +0000

    1887 beats 73%.

--- 1887_Reduction_Operations_to_Make_the_Array_Elements_Equal/1887.cc
+++ 1887_Reduction_Operations_to_Make_the_Array_Elements_Equal/1887.cc
@@ -8,11 +8,16 @@ using std::vector;
 class Solution {
  public:
   int reductionOperations(const vector<int>& nums) {
-    vector<int> num_coll(50001);
-    for (const int num : nums) ++num_coll[num];
+    int min_num = INT_MAX, max_num = INT_MIN;
+    for (const int num : nums) {
+      if (num < min_num) min_num = num;
+      if (max_num < num) max_num = num;
+    }
+    vector<int> num_coll(max_num - min_num + 1);
+    for (const int num : nums) ++num_coll[num - min_num];
 
     int answer = 0;
-    for (int i = 50000, curr_num_count = 0; 0 <= i; --i)
+    for (int i = num_coll.size() - 1, curr_num_count = 0; 0 <= i; --i)
       if (num_coll[i] != 0) {
         answer += curr_num_count;
         curr_num_count += num_coll[i];
