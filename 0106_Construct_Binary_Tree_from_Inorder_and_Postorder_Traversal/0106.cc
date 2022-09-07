b1dfcfd15c0e787200a00fa60ae7f0848655aef5
Lucius Kyle <luciuskyle@outlook.com>
2022-09-07 07:31:51 +0000

    0105 and 0106 solved.

--- 0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/0106.cc
+++ 0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/0106.cc
@@ -1,4 +1,6 @@
 
+#include <limits.h>
+
 #include <vector>
 
 using std::vector;
@@ -6,76 +8,94 @@ using std::vector;
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
-  TreeNode* left;
-  TreeNode* right;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution_0105 {
  public:
-  TreeNode* buildTree(const vector<int>& preorder, const vector<int>& inorder) {
-    if (preorder.empty()) return nullptr;
-    if (preorder.size() == 1) return new TreeNode(preorder[0]);
-    TreeNode* root = new TreeNode(preorder.front());
-    size_t left_tree_count = 0;
-    for (const int val : inorder) {
-      if (val == preorder.front()) break;
-      ++left_tree_count;
+  TreeNode *buildTree(const vector<int> &preorder, const vector<int> &inorder) {
+    preorder_ptr_ = &preorder;
+    inorder_ptr_ = &inorder;
+    int max_val = INT_MIN;
+    for (const int val : inorder) {  // this for loop is time consuming.
+      if (val < min_val_) min_val_ = val;
+      if (max_val < val) max_val = val;
     }
-    root->left = buildTree(
-        {preorder.cbegin() + 1, preorder.cbegin() + 1 + left_tree_count},
-        {inorder.cbegin(), inorder.cbegin() + left_tree_count});
-    root->right =
-        buildTree({preorder.cbegin() + 1 + left_tree_count, preorder.cend()},
-                  {inorder.cbegin() + 1 + left_tree_count, inorder.cend()});
-    return root;
+    inorder_index_vec_.resize(max_val - min_val_ + 1);
+    for (int i = 0; i < inorder.size(); ++i)
+      inorder_index_vec_[inorder[i] - min_val_] = i;
+
+    return buildTree(0, 0, inorder.size() - 1);
   }
-  // TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {}
+
+ private:
+  int min_val_ = INT_MAX;
+  const vector<int> *preorder_ptr_ = nullptr;
+  const vector<int> *inorder_ptr_ = nullptr;
+  vector<int> inorder_index_vec_;
+
+  TreeNode *buildTree(const int preorder_index, const int start_idx, const int end_idx) {
+    TreeNode *node = new TreeNode((*preorder_ptr_)[preorder_index]);
+    const int inorder_index = inorder_index_vec_[(*preorder_ptr_)[preorder_index] - min_val_];
+    if (inorder_index != start_idx)
+      node->left = buildTree(preorder_index + 1, start_idx, inorder_index - 1);
+    if (inorder_index != end_idx)
+      node->right = buildTree(preorder_index + 1 + inorder_index - start_idx, inorder_index + 1, end_idx);
+
+    return node;
+  }
 };
 
 class Solution {
  public:
-  // TreeNode* buildTree(const vector<int>& preorder, const vector<int>&
-  // inorder) {
-  //   if (preorder.empty()) return nullptr;
-  //   if (preorder.size() == 1) return new TreeNode(preorder[0]);
-  //   TreeNode* root = new TreeNode(preorder.front());
-  //   size_t left_tree_count = 0;
-  //   for (const int val : inorder) {
-  //     if (val == preorder.front()) break;
-  //     ++left_tree_count;
-  //   }
-  //   root->left = buildTree(
-  //       {preorder.cbegin() + 1, preorder.cbegin() + 1 + left_tree_count},
-  //       {inorder.cbegin(), inorder.cbegin() + left_tree_count});
-  //   root->right =
-  //       buildTree({preorder.cbegin() + 1 + left_tree_count, preorder.cend()},
-  //                 {inorder.cbegin() + 1 + left_tree_count, inorder.cend()});
-  //   return root;
-  // }
-
-  // this function won't pass leetcode test. stack-overflow.
-  TreeNode* buildTree(const vector<int>& inorder, const vector<int>& postorder) {
-    if (postorder.empty()) return nullptr;
-    if (postorder.size() == 1) return new TreeNode(postorder.front());
-    TreeNode* root = new TreeNode(postorder.back());
-    size_t left_tree_count = 0;
-    for (const int val : inorder) {
-      if (val == postorder.front()) break;
-      ++left_tree_count;
+  TreeNode *buildTree(const vector<int> &inorder, const vector<int> &postorder) {
+    postorder_ptr_ = &postorder;
+    inorder_ptr_ = &inorder;
+    int max_val = INT_MIN;
+    for (const int val : inorder) {  // this for loop is time consuming.
+      if (val < min_val_) min_val_ = val;
+      if (max_val < val) max_val = val;
     }
-    root->left = buildTree(
-        {preorder.cbegin() + 1, preorder.cbegin() + 1 + left_tree_count},
-        {inorder.cbegin(), inorder.cbegin() + left_tree_count});
-    root->right =
-        buildTree({preorder.cbegin() + 1 + left_tree_count, preorder.cend()},
-                  {inorder.cbegin() + 1 + left_tree_count, inorder.cend()});
-    return root;
+    inorder_index_vec_.resize(max_val - min_val_ + 1);
+    for (int i = 0; i < inorder.size(); ++i)
+      inorder_index_vec_[inorder[i] - min_val_] = i;
+    return buildTree(postorder.size() - 1, 0, inorder.size() - 1);
+  }
+
+ private:
+  int min_val_ = INT_MAX;
+  const vector<int> *postorder_ptr_ = nullptr;
+  const vector<int> *inorder_ptr_ = nullptr;
+  vector<int> inorder_index_vec_;
+
+  TreeNode *buildTree(const int postorder_idx, const int inorder_start_idx, const int inorder_end_idx) {
+    TreeNode *node = new TreeNode((*postorder_ptr_)[postorder_idx]);
+    const int inorder_index = inorder_index_vec_[(*postorder_ptr_)[postorder_idx] - min_val_];
+    if (inorder_index != inorder_end_idx)
+      node->right = buildTree(postorder_idx - 1, inorder_index + 1, inorder_end_idx);
+    if (inorder_index != inorder_start_idx)
+      node->left = buildTree(postorder_idx - 1 - (inorder_end_idx - inorder_index), inorder_start_idx, inorder_index - 1);
+
+    return node;
   }
 };
 
+void freeTree(TreeNode *node) {
+  if (node == nullptr) return;
+  freeTree(node->left);
+  freeTree(node->right);
+  delete node;
+}
+
 int main(void) {
   Solution sln;
-  
+  TreeNode *temp_root = sln.buildTree({15, 20, 7}, {15, 7, 20});
+  TreeNode *root = sln.buildTree({9, 3, 15, 20, 7}, {9, 15, 7, 20, 3});
+  freeTree(root);
+  freeTree(temp_root);
   return 0;
 }
