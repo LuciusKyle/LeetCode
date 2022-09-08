46ef54a9b886e8226a0751616e78213f51811fff
LuciusKyle <LuciusKyle@outlook.com>
2022-09-08 14:30:56 +0000

    0094 0144 and 0145 solved.

--- 0094_Binary_Tree_Inorder_Traversal/0094.cc
+++ 0094_Binary_Tree_Inorder_Traversal/0094.cc
@@ -1,6 +1,8 @@
 
+#include <stack>
 #include <vector>
 
+using std::stack;
 using std::vector;
 
 // Definition for a binary tree node.
@@ -8,44 +10,70 @@ struct TreeNode {
   int val;
   TreeNode *left;
   TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
  public:
+  // 0144: https://leetcode.com/problems/binary-tree-preorder-traversal
   vector<int> preorderTraversal(TreeNode *root) {
-    vector<int> rtn;
-    preorderTraversal(root, rtn);
-    return rtn;
-  }
-  vector<int> postorderTraversal(TreeNode *root) {
-    vector<int> rtn;
-    postorderTraversal(root, rtn);
-    return rtn;
-  }
-  vector<int> inorderTraversal(TreeNode *root) {
-    vector<int> rtn;
-    inorderTraversal(root, rtn);
-    return rtn;
+    vector<int> result_vec;
+    stack<TreeNode *> node_stk;
+    TreeNode *curr = root;
+    while (curr || !node_stk.empty())
+      if (curr != nullptr) {
+        result_vec.push_back(curr->val);
+        node_stk.push(curr);
+        curr = curr->left;
+      } else {
+        curr = node_stk.top()->right;
+        node_stk.pop();
+      }
+
+    return result_vec;
   }
 
- private:
-  void preorderTraversal(TreeNode *node, vector<int> &rtn) {
-    if (node == nullptr) return;
-    rtn.push_back(node->val);
-    preorderTraversal(node->left, rtn);
-    preorderTraversal(node->right, rtn);
+  // 0145: https://leetcode.com/problems/binary-tree-postorder-traversal
+  vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> result_vec;
+    stack<TreeNode *> node_stk;
+    TreeNode *curr = root;
+    while (curr != nullptr || !node_stk.empty())
+      if (curr != nullptr) {
+        node_stk.push(curr);
+        node_stk.push(curr);
+        curr = curr->left;
+      } else {
+        curr = node_stk.top();
+        node_stk.pop();
+        if (node_stk.empty() || curr != node_stk.top()) {
+          result_vec.push_back(curr->val);
+          curr = nullptr;
+        } else {
+          curr = curr->right;
+        }
+      }
+
+    return result_vec;
   }
-  void postorderTraversal(TreeNode *node, vector<int> &rtn) {
-    if (node == nullptr) return;
-    postorderTraversal(node->left, rtn);
-    postorderTraversal(node->right, rtn);
-    rtn.push_back(node->val);
-  }
-  void inorderTraversal(TreeNode *node, vector<int> &rtn) {
-    if (node == nullptr) return;
-    inorderTraversal(node->left, rtn);
-    rtn.push_back(node->val);
-    inorderTraversal(node->right, rtn);
+
+  // 0094: https://leetcode.com/problems/binary-tree-inorder-traversal
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result_vec;
+    stack<TreeNode *> node_stk;
+    TreeNode *curr = root;
+    while (curr || !node_stk.empty())
+      if (curr != nullptr) {
+        node_stk.push(curr);
+        curr = curr->left;
+      } else {
+        result_vec.push_back(node_stk.top()->val);
+        curr = node_stk.top()->right;
+        node_stk.pop();
+      }
+
+    return result_vec;
   }
 };
