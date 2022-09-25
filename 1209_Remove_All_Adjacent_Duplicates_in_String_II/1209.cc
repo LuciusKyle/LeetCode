31e4a5093648c9eb806dad6324d99f5395e820a4
Lucius Kyle <luciuskyle@outlook.com>
2022-09-25 00:25:34 +0000

    1209 solved.

--- 1209_Remove_All_Adjacent_Duplicates_in_String_II/1209.cc
+++ 1209_Remove_All_Adjacent_Duplicates_in_String_II/1209.cc
@@ -17,8 +17,7 @@ class Solution {
     }
     s.clear();  // s is reversed answer.
     while (!str_stk.empty()) {
-      for (int i = 0; i < str_stk.top().second; ++i)
-        s.push_back(str_stk.top().first);
+      s.append(str_stk.top().second, str_stk.top().first);
       str_stk.pop();
     }
     return {s.crbegin(), s.crend()};
