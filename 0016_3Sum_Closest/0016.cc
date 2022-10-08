1306d85446a24640c7d599f8ecf4887da6b84f0e
LuciusKyle <LuciusKyle@outlook.com>
2022-10-08 02:02:05 +0000

    0016 solved.

--- 0016_3Sum_Closest/0016.cc
+++ 0016_3Sum_Closest/0016.cc
@@ -9,10 +9,10 @@ class Solution {
  public:
   int threeSumClosest(vector<int>& nums, const int target) {
     std::sort(nums.begin(), nums.end());
-    int closest_sum = nums[0] + nums[1] + nums[2];
-    int closest_distance = abs(closest_sum - target);
-    for (int i = 0; i + 2 < nums.size(); ++i) {
-      int l_index = i + 1, r_index = nums.size() - 1;
+    int closest_sum = nums[0] + nums[1] + nums[2], closest_distance = abs(closest_sum - target);
+    const int length = nums.size();
+    for (int i = 0; i < length - 2; ++i) {
+      int l_index = i + 1, r_index = length - 1;
       while (l_index < r_index) {
         const int temp_sum = nums[i] + nums[l_index] + nums[r_index];
         if (abs(temp_sum - target) < closest_distance) {
