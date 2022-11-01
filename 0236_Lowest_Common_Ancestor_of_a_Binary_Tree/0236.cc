80a43c6f743e23e5bb1e015103924fb319695d1a
LuciusKyle <LuciusKyle@outlook.com>
2022-11-01 16:44:10 +0000

    0236 solved.

--- 0236_Lowest_Common_Ancestor_of_a_Binary_Tree/0236.cc
+++ 0236_Lowest_Common_Ancestor_of_a_Binary_Tree/0236.cc
@@ -1,45 +1,36 @@
 
 #include <set>
+#include <stack>
 #include <vector>
 
-using std::vector;
 using std::set;
+using std::stack;
+using std::vector;
 
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
   TreeNode* left;
   TreeNode* right;
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
  public:
+  Solution() : answer_(nullptr) {}
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    vector<TreeNode*> first_traversal;
-
-    return nullptr;
+    lowestCommonAncestor_helper(root, p, q);
+    return answer_;
   }
 
  private:
-  int preOrderTraversal(TreeNode* node, vector<TreeNode *>& ptr_vec, const TreeNode* p, const TreeNode* q, int count) {
-    if (node == nullptr) return;
-    if (node->left == p || node->right == q)
-    ptr_vec.push_back(node);
-    preOrderTraversal(node->left, ptr_vec, p, q, count);
-    preOrderTraversal(node->right, ptr_vec, p, q, count);
-  }
-
-  void inOrderTraversal(TreeNode* node, set<TreeNode *>& in_order_traversal, const TreeNode* p, const TreeNode* q, bool start_record) {
-    if (node == nullptr) return;
-    inOrderTraversal(node->left, in_order_traversal, p, q, start_record);
-    if (node == p || node == q) {
-      if (start_record)
-        return;
-      else
-        start_record = true;
-    }
-    in_order_traversal.insert(node);
-    inOrderTraversal(node->right, in_order_traversal, p, q, start_record);
+  TreeNode* answer_;
+  int lowestCommonAncestor_helper(TreeNode* node, const TreeNode* p, const TreeNode* q) {
+    if (node == nullptr) return 0;
+    int result = node == p || node == q ? 1 : 0;
+    int left_result = lowestCommonAncestor_helper(node->left, p, q);
+    int right_result = lowestCommonAncestor_helper(node->right, p, q);
+    if (result + left_result + right_result == 2) answer_ = node;
+    return (result + left_result + right_result) > 0 ? 1 : 0;
   }
 };
