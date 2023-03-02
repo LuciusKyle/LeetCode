ff21fdd8acec71d501e9b6863fb81e9618302bfc
LuciusKyle <LuciusKyle@outlook.com>
2023-03-02 00:54:54 -0800

    0443 solved. 0 ms, beats 100%.

--- 0443_String_Compression/0443.cc
+++ 0443_String_Compression/0443.cc
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 #include <string.h>
 
 #include <string>
@@ -10,28 +11,37 @@ using std::vector;
 class Solution {
  public:
   int compress(vector<char>& chars) {
-    char pre_char = chars[0];
+    char pre_char = chars[0], buf[8] = {'\0'};
     short char_repeation = 1, compressed_index = 0;
     for (int i = 1; i < chars.size(); ++i) {
       if (chars[i] != pre_char) {
         chars[compressed_index++] = pre_char;
         if (char_repeation != 1) {
-          const string repeations = std::to_string(char_repeation);
-          memcpy(chars.data() + compressed_index, repeations.data(), repeations.size());
-          compressed_index += repeations.size();
-          char_repeation = 1;
+          const int str_len = char_repeation < 10 ? 1 : (char_repeation < 100 ? 2 : (char_repeation < 1000 ? 3 : 4));
+          mini_itoa(buf, char_repeation, str_len);
+          memcpy(chars.data() + compressed_index, buf, str_len);
+          compressed_index += str_len;
         }
+        char_repeation = 0;
         pre_char = chars[i];
-      } else {
-        ++char_repeation;
       }
+      ++char_repeation;
     }
     chars[compressed_index++] = pre_char;
     if (char_repeation != 1) {
-      const string repeations = std::to_string(char_repeation);
-      memcpy(chars.data() + compressed_index, repeations.data(), repeations.size());
-      compressed_index += repeations.size();
+      const int str_len = char_repeation < 10 ? 1 : (char_repeation < 100 ? 2 : (char_repeation < 1000 ? 3 : 4));
+      mini_itoa(buf, char_repeation, str_len);
+      memcpy(chars.data() + compressed_index, buf, str_len);
+      compressed_index += str_len;
     }
     return compressed_index;
+  }
+
+ private:
+  void mini_itoa(char* str, int val, const int str_len) {
+    for (int i = str_len - 1; 0 <= i; --i) {
+      str[i] = '0' + val % 10;
+      val /= 10;
+    }
   }
 };
