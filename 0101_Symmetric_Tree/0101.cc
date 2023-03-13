8e6702e9326cef37aa2851f777b40455ed2d72c4
LuciusKyle <LuciusKyle@outlook.com>
2023-03-13 03:04:47 -0700

    0100 and 0101 solved.

--- 0101_Symmetric_Tree/0101.cc
+++ 0101_Symmetric_Tree/0101.cc
@@ -1,33 +1,25 @@
 
-
 // Definition for a binary tree node.
 struct TreeNode {
   int val;
-  TreeNode* left;
-  TreeNode* right;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
  public:
-  bool isSameTree(TreeNode* p, TreeNode* q) {  // problem 100.
-    if ((p == nullptr) && (q == nullptr)) return true;
-    if ((p == nullptr) || (q == nullptr)) return false;
-    if (p->val != q->val) return false;
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-  }
-
-  bool isSymmetric(TreeNode* root) {  // problem 101.
-    if (root == nullptr) return true;
+  bool isSymmetric(TreeNode *root) {
     return isSymmetric(root->left, root->right);
   }
 
  private:
-  bool isSymmetric(TreeNode* p, TreeNode* q) {
-    if ((p == nullptr) && (q == nullptr)) return true;
-    if ((p == nullptr) || (q == nullptr)) return false;
+  bool isSymmetric(TreeNode *p, TreeNode *q) {
+    if (p == q && p == nullptr) return true;
+    if (p == nullptr || q == nullptr) return false;
     if (p->val != q->val) return false;
     return isSymmetric(p->left, q->right) && isSymmetric(p->right, q->left);
   }
 };
-
