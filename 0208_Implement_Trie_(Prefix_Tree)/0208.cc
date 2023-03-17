09afc9a22bda9515ceb21e978885e81bcdc18fd5
LuciusKyle <LuciusKyle@outlook.com>
2023-03-17 03:51:23 -0700

    0208 solved.

--- 0208_Implement_Trie_(Prefix_Tree)/0208.cc
+++ 0208_Implement_Trie_(Prefix_Tree)/0208.cc
@@ -1,50 +1,61 @@
 
-#include <memory>
 #include <string>
 #include <vector>
 
 using std::string;
+using std::vector;
 
 class Trie {
  public:
-  Trie() : root_(std::make_shared<Node>()) {}
+  Trie() : root_(new Node) {}
+  ~Trie() {
+    freeNodes(root_);
+    delete root_;
+  }
 
   void insert(const string word) {
-    std::shared_ptr<Node> temp_node_ptr = root_;
+    Node* temp_ptr = root_;
     for (const char ch : word) {
-      if (temp_node_ptr->child_nodes[ch - 'a'] == nullptr)
-        temp_node_ptr->child_nodes[ch - 'a'] = std::make_shared<Node>();
-
-      temp_node_ptr = temp_node_ptr->child_nodes[ch - 'a'];
+      if (temp_ptr->nodes[ch - 'a'] == nullptr) temp_ptr->nodes[ch - 'a'] = new Node;
+      temp_ptr = temp_ptr->nodes[ch - 'a'];
     }
-    temp_node_ptr->end_of_word = true;
+    temp_ptr->end_of_word = true;
   }
 
-  bool search(const string word) {
-    std::shared_ptr<Node> temp_node_ptr = root_;
+  bool search(const string word) const {
+    Node* temp_ptr = root_;
     for (const char ch : word) {
-      if (temp_node_ptr->child_nodes[ch - 'a'] == nullptr) return false;
-      temp_node_ptr = temp_node_ptr->child_nodes[ch - 'a'];
+      if (temp_ptr->nodes[ch - 'a'] == nullptr) return false;
+      temp_ptr = temp_ptr->nodes[ch - 'a'];
     }
-    return temp_node_ptr->end_of_word;
+    return temp_ptr->end_of_word;
   }
 
-  bool startsWith(const string prefix) {
-    std::shared_ptr<Node> temp_node_ptr = root_;
+  bool startsWith(const string prefix) const {
+    Node* temp_ptr = root_;
     for (const char ch : prefix) {
-      if (temp_node_ptr->child_nodes[ch - 'a'] == nullptr) return false;
-      temp_node_ptr = temp_node_ptr->child_nodes[ch - 'a'];
+      if (temp_ptr->nodes[ch - 'a'] == nullptr) return false;
+      temp_ptr = temp_ptr->nodes[ch - 'a'];
     }
     return true;
   }
 
  private:
   struct Node {
-    Node() : end_of_word(false), child_nodes{nullptr} {};
+    Node() : end_of_word(false), nodes{nullptr} {};
     bool end_of_word;
-    std::shared_ptr<Node> child_nodes[26];
+    Node* nodes[26];
   };
-  std::shared_ptr<Node> root_;
+
+  void freeNodes(Node* node) {
+    for (Node* child_node : node->nodes) {
+      if (child_node != nullptr) {
+        freeNodes(child_node);
+        delete child_node;
+      }
+    }
+  }
+  Node* root_;
 };
 
 /**
