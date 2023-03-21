cd3b4f2266c2e3e7b0e013e3bb034c97426fbbb5
LuciusKyle <LuciusKyle@outlook.com>
2023-03-21 20:22:34 +0000

    0878 solved.

--- 0878_Nth_Magical_Number/0878.cc
+++ 0878_Nth_Magical_Number/0878.cc
@@ -1,16 +1,30 @@
 
+#include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
 using std::vector;
+
+constexpr int kModulo = 1'000'000'007;
 
 class Solution {
  public:
   int nthMagicalNumber(int n, const int a, const int b) {
-    const int common_multiple = leaseCommonMultiple(a, b);
-    int answer = (n / int(number_base_.size())) * common_multiple;
+    const int common_multiple = leaseCommonMultiple(a, b) % kModulo;
+    const int max_step = kModulo / common_multiple;
+    int steps = n / number_base_.size();
+    int answer = 0;
+    while (max_step < steps) {
+      answer += max_step * common_multiple;
+      answer %= kModulo;
+      steps -= max_step;
+    }
+    answer += steps * common_multiple;
+    answer %= kModulo;
     n %= number_base_.size();
     if (n == 0) return answer;
-    return answer + number_base_[n - 1];
+    return (answer + (number_base_[n - 1] % kModulo)) % kModulo;
   }
 
  private:
@@ -32,5 +46,6 @@ class Solution {
 int main(int argc, char* argv[]) {
   Solution sln;
   sln.nthMagicalNumber(4, 2, 3);
+  cout << sln.nthMagicalNumber(1000000000, 39999, 40000) << endl;
   return 0;
 }
