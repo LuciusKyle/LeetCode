f88bc3d3ec2a522f72dc6a07cb6decd941d00707
Lucius Kyle <LuciusKyle@outlook.com>
2023-04-06 13:01:41 +0000

    0229 solved.

--- 0229_Majority_Element_II/0229.cc
+++ 0229_Majority_Element_II/0229.cc
@@ -1,14 +1,19 @@
 
+#include <map>
 #include <vector>
 
+using std::map;
 using std::vector;
 
 class Solution {
  public:
   vector<int> majorityElement(vector<int>& nums) {
     vector<int> rtn;
-    if (nums.empty()) return rtn;
-    
+    map<int, int> val_freq_map;
+    for (const int val : nums)
+      ++val_freq_map[val];
+    for (auto iter = val_freq_map.cbegin(); iter != val_freq_map.cend(); ++iter)
+      if (iter->second > nums.size() / 3) rtn.push_back(iter->first);
+    return rtn;
   }
 };
-
