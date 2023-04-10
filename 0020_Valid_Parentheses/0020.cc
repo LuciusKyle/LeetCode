450206ae1991e5d230f9d41c4dcc754b7ba6d47e
Lucius Kyle <LuciusKyle@outlook.com>
2023-04-10 02:34:32 +0000

    0020 solved.

--- 0020_Valid_Parentheses/0020.cc
+++ 0020_Valid_Parentheses/0020.cc
@@ -1,44 +1,41 @@
 
+#include <assert.h>
+
 #include <stack>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include<assert.h>
 
 using std::stack;
 using std::string;
 
 class Solution {
  public:
-  Solution() : parentheses{'(', '{', '['}, reverse_parentheses{')', '}', ']'} {
-    parenthesis_pairs.insert({')', '('});
-    parenthesis_pairs.insert({'}', '{'});
-    parenthesis_pairs.insert({']', '['});
-  }
-
-  bool isValid(string s) {
-    std::stack<char> parentheses_stack;
-    for (const char ch : s) {
-      if (parentheses.count(ch) > 0) {
-        parentheses_stack.push(ch);
-        continue;
+  bool isValid(const string s) {
+    stack<char> stk;
+    for (const char ch : s)
+      switch (ch) {
+        case '(':
+        case '{':
+        case '[':
+          stk.push(ch);
+          break;
+        case ')':
+          if (stk.empty() || stk.top() != '(') return false;
+          stk.pop();
+          break;
+        case '}':
+          if (stk.empty() || stk.top() != '{') return false;
+          stk.pop();
+          break;
+        case ']':
+          if (stk.empty() || stk.top() != '[') return false;
+          stk.pop();
+          break;
+        default:
+          abort();
+          break;
       }
-      if (reverse_parentheses.count(ch) > 0) {
-        if (parentheses_stack.empty()) return false;
-        if (parentheses_stack.top() != parenthesis_pairs.at(ch)) return false;
-        parentheses_stack.pop();
-      }
-    }
-    if (parentheses_stack.empty())
-      return true;
-    else
-      return false;
+    return stk.empty();
   }
-
- private:
-  const std::unordered_set<char> parentheses;
-  const std::unordered_set<char> reverse_parentheses;
-  std::unordered_map<char, char> parenthesis_pairs;
 };
 
 int main(void) {
