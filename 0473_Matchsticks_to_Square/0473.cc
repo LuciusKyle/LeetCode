8b1e0267c8b73651903162a15ef4d107267a4e37
Lucius Kyle <LuciusKyle@outlook.com>
2023-04-13 14:20:36 +0800

    0473 solved.

--- 0473_Matchsticks_to_Square/0473.cc
+++ 0473_Matchsticks_to_Square/0473.cc
@@ -10,18 +10,28 @@ class Solution {
     const int all_sum = std::accumulate(matchsticks.cbegin(), matchsticks.cend(), 0);
     if (all_sum % 4 != 0) return false;
     all_sum_ = all_sum;
+    all_used_sticks_.resize(1 << matchsticks.size());
     return makeSquare(matchsticks, 0, 0, 0, all_sum / 4);
   }
 
  private:
   int all_sum_;
+  vector<int> all_used_sticks_;
   bool makeSquare(const vector<int>& matchsticks, const int curr_edge, const int start_index, const int used_sticks, const int target) {
-    if (curr_edge == 3) return true;
+    if (all_used_sticks_[used_sticks] != 0) return all_used_sticks_[used_sticks] == 1;
+    if (curr_edge == 3) {
+      all_used_sticks_[used_sticks] = 1;
+      return true;
+    }
     for (int i = start_index; i < matchsticks.size(); ++i) {
       if (!(used_sticks & (1 << i)) && matchsticks[i] <= target) {
-        if (makeSquare(matchsticks, matchsticks[i] == target ? curr_edge + 1 : curr_edge, matchsticks[i] == target ? 0 : i + 1, used_sticks | 1 << i, matchsticks[i] == target ? all_sum_ / 4 : target - matchsticks[i])) return true;
+        if (makeSquare(matchsticks, matchsticks[i] == target ? curr_edge + 1 : curr_edge, matchsticks[i] == target ? 0 : i + 1, used_sticks | 1 << i, matchsticks[i] == target ? all_sum_ / 4 : target - matchsticks[i])) {
+          all_used_sticks_[used_sticks] = 1;
+          return true;
+        }
       }
     }
+    all_used_sticks_[used_sticks] = -1;
     return false;
   }
 };
