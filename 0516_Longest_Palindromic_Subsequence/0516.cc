b684633a8b0d21c8ad5b41a66a2941f95e6b470b
LuciusKyle <LuciusKyle@outlook.com>
2023-04-14 05:53:37 -0700

    0516 beats 31%.

--- 0516_Longest_Palindromic_Subsequence/0516.cc
+++ 0516_Longest_Palindromic_Subsequence/0516.cc
@@ -8,22 +8,34 @@ using std::vector;
 class Solution {
  public:
   int longestPalindromeSubseq(const string s) {
-    vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
-    return longestPalindromeSubseq(s, 0, s.size() - 1, dp);
+    const int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+      dp[i][i] = 1;
+      if (i != 0)
+        dp[i - 1][i] = s[i - 1] == s[i] ? 2 : 1;
+      if (i != n - 1)
+        dp[i][i + 1] = s[i] == s[i + 1] ? 2 : 1;
+    }
+    // return longestPalindromeSubseq(s, 0, n - 1, dp); // faster, but still beats 5%.
+    for (int len = 2; len < n; ++len) {
+      for (int start = 0; start + len < n; ++start) {
+        dp[start][start + len] = std::max(dp[start + 1][start + len - 1] + (s[start] == s[start + len] ? 2 : 0), std::max(dp[start][start + len - 1], dp[start + 1][start + len]));
+      }
+    }
+    return dp[0][n - 1];
   }
 
  private:
   int longestPalindromeSubseq(const string &s, int start, int end, vector<vector<int>> &dp) {
     if (dp[start][end] != -1) return dp[start][end];
-    if (start == end) {
-      dp[start][end] = 1;
-      return 1;
-    }
-    if (start + 1 == end) {
-      dp[start][end] = s[start] == s[end] ? 2 : 1;
-      return dp[start][end];
-    }
     dp[start][end] = std::max(longestPalindromeSubseq(s, start + 1, end - 1, dp) + (s[start] == s[end] ? 2 : 0), std::max(longestPalindromeSubseq(s, start + 1, end, dp), longestPalindromeSubseq(s, start, end - 1, dp)));
     return dp[start][end];
   }
 };
+
+int main(int argc, char *argv[]) {
+  Solution sln;
+  sln.longestPalindromeSubseq("aa");
+  return 0;
+}
