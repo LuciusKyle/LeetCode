2075dbdddbd20b7082035750b8dfccbba1722ab5
LuciusKyle <LuciusKyle@outlook.com>
2023-04-16 05:35:43 -0700

    1639 beats 64%.

--- 1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary/1639.cc
+++ 1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary/1639.cc
@@ -5,6 +5,8 @@
 using std::string;
 using std::vector;
 
+constexpr int kModulo = 1000000007;
+
 class Solution {
  public:
   int numWays(const vector<string>& words, const string target) {
@@ -15,13 +17,13 @@ class Solution {
         ++ch_count[j][words[i][j] - 'a'];
       }
     }
-    vector<vector<int>> answer(candidate_length, vector<int>(target.size(), 0));
+    vector<vector<long>> answer(candidate_length, vector<long>(target.size(), 0));
     answer[0][0] = ch_count[0][target[0] - 'a'];
-    for (int i = 1, prefix_sum = 0; i < candidate_length; ++i)
+    for (int i = 1, prefix_sum = 0; i < candidate_length; ++i)  // no need to "% kModulo"
       answer[i][0] = answer[i - 1][0] + ch_count[i][target[0] - 'a'];
     for (int i = 1; i < target.size(); ++i) {
       for (int j = i; j < candidate_length; ++j) {
-        answer[j][i] = ch_count[j][target[i] - 'a'] * answer[j - 1][i - 1] + answer[j - 1][i];
+        answer[j][i] = ((ch_count[j][target[i] - 'a'] * answer[j - 1][i - 1]) % kModulo + answer[j - 1][i]) % kModulo;
       }
     }
     return answer.back().back();
@@ -32,5 +34,7 @@ int main(int argc, char* argv[]) {
   Solution sln;
   sln.numWays({"abba", "baab"}, "bab");
   sln.numWays({"acca", "bbbb", "caca"}, "aba");
+  sln.numWays({"cbabddddbc", "addbaacbbd", "cccbacdccd", "cdcaccacac", "dddbacabbd", "bdbdadbccb", "ddadbacddd", "bbccdddadd", "dcabaccbbd", "ddddcddadc", "bdcaaaabdd", "adacdcdcdd", "cbaaadbdbb", "bccbabcbab", "accbdccadd", "dcccaaddbc", "cccccacabd", "acacdbcbbc", "dbbdbaccca", "bdbddbddda", "daabadbacb", "baccdbaada", "ccbabaabcb", "dcaabccbbb", "bcadddaacc", "acddbbdccb", "adbddbadab", "dbbcdcbcdd", "ddbabbadbb", "bccbcbbbab", "dabbbdbbcb", "dacdabadbb", "addcbbabab", "bcbbccadda", "abbcacadac", "ccdadcaada", "bcacdbccdb"}, "bcbbcccc");
+  sln.numWays({"cabbaacaaaccaabbbbaccacbabbbcb", "bbcabcbcccbcacbbbaacacaaabbbac", "cbabcaacbcaaabbcbaabaababbacbc", "aacabbbcaaccaabbaccacabccaacca", "bbabbaabcaabccbbabccaaccbabcab", "bcaccbbaaccaabcbabbacaccbbcbbb", "cbbcbcaaaacacabbbabacbaabbabaa", "cbbbbbbcccbabbacacacacccbbccca", "bcbccbccacccacaababcbcbbacbbbc", "ccacaabaaabbbacacbacbaaacbcaca", "bacaaaabaabccbcbbaacacccabbbcb", "bcbcbcabbccabacbcbcaccacbcaaab", "babbbcccbbbbbaabbbacbbaabaabcc", "baaaacaaacbbaacccababbaacccbcb", "babbaaabaaccaabacbbbacbcbababa", "cbacacbacaaacbaaaabacbbccccaca", "bcbcaccaabacaacaaaccaabbcacaaa", "cccbabccaabbcbccbbabaaacbacaaa", "bbbcabacbbcabcbcaaccbcacacccca", "ccccbbaababacbabcaacabaccbabaa", "caaabccbcaaccabbcbcaacccbcacba", "cccbcaacbabaacbaaabbbbcbbbbcbb", "cababbcacbabcbaababcbcabbaabba", "aaaacacaaccbacacbbbbccaabcccca", "cbcaaaaabacbacaccbcbcbccaabaac", "bcbbccbabaccabcccacbbaacbbcbba", "cccbabbbcbbabccbbabbbbcaaccaab", "acccacccaabbcaccbcaaccbababacc", "bcacabaacccbbcbbacabbbbbcaaaab", "cacccaacbcbccbabccabbcbabbcacc", "aacabbabcaacbaaacaabcabcaccaab", "cccacabacbabccbccaaaaabbcacbcc", "cabaacacacaaabaacaabababccbaaa", "caabaccaacccbaabcacbcbbabccabc", "bcbbccbbaaacbaacbccbcbababcacb", "bbabbcabcbbcababbbbccabaaccbca", "cacbbbccabaaaaccacbcbabaabbcba", "ccbcacbabababbbcbcabbcccaccbca", "acccabcacbcbbcbccaccaacbabcaab", "ccacaabcbbaabaaccbabcbacaaabaa", "cbabbbbcabbbbcbccabaabccaccaca", "acbbbbbccabacabcbbabcaacbbaacc", "baaababbcabcacbbcbabacbcbaaabc", "cabbcabcbbacaaaaacbcbbcacaccac"}, "acbaccacbbaaabbbabac");
   return 0;
 }
