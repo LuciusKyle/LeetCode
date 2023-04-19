bafc1ac66c4841bd983c330be1ab71bbacf5a1c8
LuciusKyle <LuciusKyle@outlook.com>
2023-04-19 05:46:50 -0700

    0053 solved.

--- 0053_Maximum_Subarray/0053.cc
+++ 0053_Maximum_Subarray/0053.cc
@@ -1,38 +1,20 @@
 
-#include <assert.h>
-#include <vector> // for std::begin() and std::end();
+#include <limits.h>
+
+#include <algorithm>
+#include <vector>
 
 using std::vector;
 
-int my_max_fun(int a, int b) { return a < b ? b : a; }
-
-int maxSubArray(int* nums, int numsSize) {
-  if (numsSize == 1) return nums[0];
-  if (numsSize == 2)
-    return my_max_fun((nums[0] + nums[1]), my_max_fun(nums[0], nums[1]));
-
-  const int mid_index = (numsSize - 1) / 2;
-  int left_max = nums[mid_index];
-  int left_sum = left_max;
-  int right_max = nums[mid_index + 1];
-  int right_sum = right_max;
-  for (int i = mid_index - 1; i >= 0; --i) {
-    left_sum += nums[i];
-    if (left_max < left_sum) left_max = left_sum;
+class Solution {
+ public:
+  int maxSubArray(const vector<int>& nums) {
+    int prefix_sum = 0, min_prefix_sum = 0, answer = nums[0];
+    for (const int val : nums) {
+      prefix_sum += val;
+      answer = std::max(answer, prefix_sum - min_prefix_sum);
+      min_prefix_sum = std::min(min_prefix_sum, prefix_sum);
+    }
+    return answer;
   }
-  for (int i = mid_index + 2; i < numsSize; ++i) {
-    right_sum += nums[i];
-    if (right_max < right_sum) right_max = right_sum;
-  }
-  return my_max_fun(right_max + left_max,
-                    my_max_fun(maxSubArray(nums, mid_index + 1),
-                               maxSubArray(nums + mid_index + 1,
-                                           numsSize - (mid_index + 1))));
-}
-
-int main(void) {
-  int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-  int rtn = maxSubArray(arr, std::end(arr) - std::begin(arr));
-  assert(rtn == 6);
-  return 0;
-}
+};
