7cabac14f09dec69c0b896b9f8e5adf4a1ab9899
Lucius Kyle <LuciusKyle@outlook.com>
2023-05-01 15:49:18 +0000

    0560 solved.

--- 0560_Subarray_Sum_Equals_K/0560.cc
+++ 0560_Subarray_Sum_Equals_K/0560.cc
@@ -10,14 +10,16 @@ using std::vector;
 class Solution {
  public:
   int subarraySum(const vector<int>& nums, int k) {
-    std::multiset<int> prefix_sum_set;
-    // prefix_sum_set.reserve(nums.size() + 32);
+    std::unordered_map<int, int> prefix_sum_map;
+    prefix_sum_map.reserve(nums.size() + 32);
     int prefix_sum = 0;
     int answer = 0;
     for (const int val : nums) {
-      prefix_sum_set.insert(prefix_sum);
+      ++prefix_sum_map[prefix_sum];
       prefix_sum += val;
-      answer += prefix_sum_set.count(prefix_sum - k);
+      const auto iter = prefix_sum_map.find(prefix_sum - k);
+      if (iter != prefix_sum_map.end())
+        answer += iter->second;
     }
     return answer;
   }
