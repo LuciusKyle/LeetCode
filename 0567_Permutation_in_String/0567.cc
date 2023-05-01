9fb44ad43360c6277a95003e97ca179f1a2c92e2
Lucius Kyle <LuciusKyle@outlook.com>
2023-05-01 16:32:31 +0000

    0567 solved.

--- 0567_Permutation_in_String/0567.cc
+++ 0567_Permutation_in_String/0567.cc
@@ -1,19 +1,25 @@
 
+#include <string.h>
+
 #include <string>
 
 using std::string;
 
 class Solution {
  public:
-  bool checkInclusion(string s1, string s2) {
-    int s2_index = 0;
-    for (int i = 0; i < s1.size(); ++i) {
-      while (s2_index < s2.size() && s1[i] != s2[s2_index]);
-      for (; s2_index < s2.size(); ++s2_index) {
-        if (s1[i] == s2[s2_index])
-          break;
-      }
-      if (s2.size() <= s2_index) return false;
+  bool checkInclusion(const string s1, const string s2) {
+    if (s2.size() < s1.size()) return false;
+    const int length1 = s1.size();
+    int dict_s2[26] = {0}, dict_s1[26] = {0};
+    for (const char ch : s1) ++dict_s1[ch - 'a'];
+    for (int i = 0; i < length1; ++i)
+      ++dict_s2[s2[i] - 'a'];
+    if (memcmp(dict_s1, dict_s2, sizeof(int) * 26) == 0) return true;
+    for (int i = length1; i < s2.size(); ++i) {
+      ++dict_s2[s2[i] - 'a'];
+      --dict_s2[s2[i - length1] - 'a'];
+      if (memcmp(dict_s1, dict_s2, sizeof(int) * 26) == 0) return true;
     }
+    return false;
   }
 };
