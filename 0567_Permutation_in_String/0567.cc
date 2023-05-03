2bdf28fe12330635e13ee1f2a423d90f4f8426e4
Lucius Kyle <LuciusKyle@outlook.com>
2023-05-03 18:18:49 +0000

    0567 solved.

--- 0567_Permutation_in_String/0567.cc
+++ 0567_Permutation_in_String/0567.cc
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include <string>
+#include <type_traits>
 
 using std::string;
 
@@ -10,15 +11,15 @@ class Solution {
   bool checkInclusion(const string s1, const string s2) {
     if (s2.size() < s1.size()) return false;
     const int length1 = s1.size();
-    int dict_s2[26] = {0}, dict_s1[26] = {0};
+    short dict_s2[26] = {0}, dict_s1[26] = {0};
     for (const char ch : s1) ++dict_s1[ch - 'a'];
     for (int i = 0; i < length1; ++i)
       ++dict_s2[s2[i] - 'a'];
-    if (memcmp(dict_s1, dict_s2, sizeof(int) * 26) == 0) return true;
+    if (memcmp(dict_s1, dict_s2, sizeof(std::remove_reference_t<decltype(*dict_s1)>) * (std::end(dict_s1) - std::begin(dict_s1))) == 0) return true;
     for (int i = length1; i < s2.size(); ++i) {
       ++dict_s2[s2[i] - 'a'];
       --dict_s2[s2[i - length1] - 'a'];
-      if (memcmp(dict_s1, dict_s2, sizeof(int) * 26) == 0) return true;
+      if (memcmp(dict_s1, dict_s2, sizeof(std::remove_reference_t<decltype(*dict_s1)>) * (std::end(dict_s1) - std::begin(dict_s1))) == 0) return true;
     }
     return false;
   }
