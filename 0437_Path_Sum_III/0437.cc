6c721c03989132a353ad0941a2d61a3734103a95
Lucius Kyle <LuciusKyle@outlook.com>
2023-05-05 07:19:17 +0000

    0437 solved.

--- 0437_Path_Sum_III/0437.cc
+++ 0437_Path_Sum_III/0437.cc
@@ -1,4 +1,7 @@
 
+#include <ctype.h>
+
+#include <unordered_map>
 #include <vector>
 
 using std::vector;
@@ -16,25 +19,24 @@ struct TreeNode {
 class Solution {
  public:
   int pathSum(TreeNode *root, int targetSum) {
-    path_prefix_sum_.push_back(0);
+    path_prefix_sum_.insert({0, 1});
     count_ = 0;
-    if (root != nullptr) pathSum_func(root, targetSum);
+    if (root != nullptr) pathSum_func(root, targetSum, 0);
     return count_;
   }
 
  private:
-  vector<long long> path_prefix_sum_;
-  long long count_;
-  void pathSum_func(const TreeNode *node, const int targetSum) {
-    const long long curr_sum = node->val + path_prefix_sum_.back();
-    for (int i = 0; i < path_prefix_sum_.size(); ++i) {
-      if (curr_sum - path_prefix_sum_[i] == targetSum) ++count_;
-    }
-    path_prefix_sum_.push_back(curr_sum);
+  std::unordered_map<long long, int> path_prefix_sum_;
+  int count_;
+  void pathSum_func(const TreeNode *node, const int targetSum, const long long prefix_sum) {
+    const long long curr_sum = node->val + prefix_sum;
+    if (path_prefix_sum_.count(curr_sum - targetSum) != 0)
+      count_ += path_prefix_sum_[curr_sum - targetSum];
+    ++path_prefix_sum_[curr_sum];
     if (node->left != nullptr)
-      pathSum_func(node->left, targetSum);
+      pathSum_func(node->left, targetSum, curr_sum);
     if (node->right != nullptr)
-      pathSum_func(node->right, targetSum);
-    path_prefix_sum_.pop_back();
+      pathSum_func(node->right, targetSum, curr_sum);
+    --path_prefix_sum_[curr_sum];
   }
 };
