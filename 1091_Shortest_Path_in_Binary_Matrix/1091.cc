4d9692052297e6fa1c210637dbfa537084e1d97a
Lucius Kyle <LuciusKyle@outlook.com>
2023-06-01 01:33:02 +0000

    1091 solved.

--- 1091_Shortest_Path_in_Binary_Matrix/1091.cc
+++ 1091_Shortest_Path_in_Binary_Matrix/1091.cc
@@ -1,4 +1,6 @@
+
 #include <assert.h>
+#include <stddef.h>
 
 #include <vector>
 
@@ -7,56 +9,28 @@ using std::vector;
 class Solution {
  public:
   int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-    const size_t n = grid.size();
+    const int n = grid.size();
     if (grid[0][0] || grid[n - 1][n - 1]) return -1;
+    grid[0][0] = -1;
     int level = 0;
-    vector<std::pair<int, int>> current_steps;
+    vector<std::pair<int, int>> current_steps, next_steps;
     current_steps.push_back({0, 0});
+    const std::pair<int, int> kEightDirections[] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     do {
-      vector<std::pair<int, int>> next_steps;
       for (const auto coordinate : current_steps) {
         if (coordinate.first == n - 1 && coordinate.second == n - 1) return level + 1;
-
-        if (0 <= coordinate.first - 1 && 0 <= coordinate.second - 1 && grid[coordinate.first - 1][coordinate.second - 1] == 0) {
-          next_steps.push_back({coordinate.first - 1, coordinate.second - 1});
-          grid[coordinate.first - 1][coordinate.second - 1] = -1;
-        }
-        if (0 <= coordinate.first - 1 && grid[coordinate.first - 1][coordinate.second] == 0) {
-          next_steps.push_back({coordinate.first - 1, coordinate.second});
-          grid[coordinate.first - 1][coordinate.second] = -1;
-        }
-        if (0 <= coordinate.second - 1 && grid[coordinate.first][coordinate.second - 1] == 0) {
-          next_steps.push_back({coordinate.first, coordinate.second - 1});
-          grid[coordinate.first][coordinate.second - 1] = -1;
-        }
-        if (coordinate.first + 1 < n && coordinate.second + 1 < n && grid[coordinate.first + 1][coordinate.second + 1] == 0) {
-          next_steps.push_back({coordinate.first + 1, coordinate.second + 1});
-          grid[coordinate.first + 1][coordinate.second + 1] = -1;
-        }
-        if (coordinate.second + 1 < n && grid[coordinate.first][coordinate.second + 1] == 0) {
-          next_steps.push_back({coordinate.first, coordinate.second + 1});
-          grid[coordinate.first][coordinate.second + 1] = -1;
-        }
-        if (coordinate.first + 1 < n && grid[coordinate.first + 1][coordinate.second] == 0) {
-          next_steps.push_back({coordinate.first + 1, coordinate.second});
-          grid[coordinate.first + 1][coordinate.second] = -1;
-        }
-        if (0 <= coordinate.first - 1 && coordinate.second + 1 < n && grid[coordinate.first - 1][coordinate.second + 1] == 0) {
-          next_steps.push_back({coordinate.first - 1, coordinate.second + 1});
-          grid[coordinate.first - 1][coordinate.second + 1] = -1;
-        }
-        if (coordinate.first + 1 < n && 0 <= coordinate.second - 1 && grid[coordinate.first + 1][coordinate.second - 1] == 0) {
-          next_steps.push_back({coordinate.first + 1, coordinate.second - 1});
-          grid[coordinate.first + 1][coordinate.second - 1] = -1;
-        }
+        for (const auto direction : kEightDirections)
+          if ((0 <= coordinate.first + direction.first) && (coordinate.first + direction.first < n) && (0 <= coordinate.second + direction.second) && (coordinate.second + direction.second < n) && (grid[coordinate.first + direction.first][coordinate.second + direction.second] == 0)) {
+            next_steps.push_back({coordinate.first + direction.first, coordinate.second + direction.second});
+            grid[coordinate.first + direction.first][coordinate.second + direction.second] = -1;
+          }
       }
-      current_steps = next_steps;
+      current_steps.swap(next_steps);
+      next_steps.clear();
       ++level;
     } while (!current_steps.empty());
     return -1;
   }
-
- private:
 };
 
 int main(void) {
