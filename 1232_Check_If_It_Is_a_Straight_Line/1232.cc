ea723bdcbc55b1d5f0617aa7dd78fd02cbee1fd1
Lucius Kyle <LuciusKyle@outlook.com>
2023-06-05 01:10:25 +0000

    1232 solved.

---
+++ 1232_Check_If_It_Is_a_Straight_Line/1232.cc
@@ -0,0 +1,21 @@
+
+#include <cmath>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+ public:
+  bool checkStraightLine(vector<vector<int>>& coordinates) {
+    if (coordinates[1][0] == coordinates[0][0]) {
+      const int x_val = coordinates[0][0];
+      for (const auto& coo : coordinates)
+        if (coo[0] != x_val) return false;
+      return true;
+    }
+    const double ratio = double(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
+    for (int i = 2; i < coordinates.size(); ++i)
+      if (0.000001 < std::abs(ratio - double(coordinates[i][1] - coordinates[0][1]) / (coordinates[i][0] - coordinates[0][0]))) return false;
+    return true;
+  }
+};
