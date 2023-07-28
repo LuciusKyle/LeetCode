3ddbe62631e948e1300a794e8525d469d21d0bc8
Lucius Kyle <LuciusKyle@outlook.com>
2023-07-28 08:34:47 +0000

    0486 beats 63%.

--- 0486_Predict_the_Winner/0486.cc
+++ 0486_Predict_the_Winner/0486.cc
@@ -1,4 +1,6 @@
 
+#include <limits.h>
+
 #include <vector>
 
 using std::vector;
@@ -6,12 +8,17 @@ using std::vector;
 class Solution {
  public:
   bool PredictTheWinner(vector<int>& nums) {
+    dp_.assign(nums.size(), vector<int>(nums.size(), INT_MIN));
+    vector<vector<int>> dp(nums.size(), vector<int>(nums.size() - 1));
     return 0 <= scoreDiff(nums, 0, nums.size() - 1);
   }
 
  private:
+  vector<vector<int>> dp_;
   int scoreDiff(const vector<int>& nums, int l_index, int r_index) {
     if (l_index == r_index) return nums[l_index];
-    return std::max(nums[l_index] - scoreDiff(nums, l_index + 1, r_index), nums[r_index] - scoreDiff(nums, l_index, r_index - 1));
+    if (dp_[l_index][r_index] == INT_MIN)
+      dp_[l_index][r_index] = std::max(nums[l_index] - scoreDiff(nums, l_index + 1, r_index), nums[r_index] - scoreDiff(nums, l_index, r_index - 1));
+    return dp_[l_index][r_index];
   }
 };
