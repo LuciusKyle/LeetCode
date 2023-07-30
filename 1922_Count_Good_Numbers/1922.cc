bdc50caafacc2f0756aeb7825e251288c933098c
Lucius Kyle <LuciusKyle@outlook.com>
2023-07-30 11:35:31 +0000

    1922 solved.

--- 1922_Count_Good_Numbers/1922.cc
+++ 1922_Count_Good_Numbers/1922.cc
@@ -1,10 +1,38 @@
 
+#include <assert.h>
+
+#include <array>
+#include <iostream>
 #include <map>
 #include <unordered_map>
 
 constexpr int kModulo = 1000000007;
 
+template <std::size_t N = 34>
+constexpr std::array<int, N> getArray() {
+  static_assert(3 <= N && N <= 62);
+  std::array<int, N> arr{5, 20};
+  for (int i = 2; i < arr.size(); ++i)
+    arr[i] = (long long)arr[i - 1] * (long long)arr[i - 1] % kModulo;
+  return arr;
+}
+
+constexpr auto kAnswerDict = getArray();
+
 class Solution {
+ public:
+  int countGoodNumbers(long long n) {
+    for (int i = kAnswerDict.size() - 1; 0 <= i; --i) {
+      long long sub_arr_len = 1ll << i;
+      if (sub_arr_len <= n) {
+        return (long long)kAnswerDict[i] * (n - sub_arr_len == 0 ? 1 : (long long)countGoodNumbers(n - sub_arr_len)) % kModulo;
+      }
+    }
+    return 1;
+  }
+};
+
+class Solution_correct {
  public:
   int countGoodNumbers(long long n) {
     if (n == 1) return 5;
@@ -19,3 +47,15 @@ class Solution {
  private:
   std::unordered_map<long long, int> answer_cache_;
 };
+
+int main(int argc, char* argv[]) {
+  Solution sln;
+  for (int i = 1; i < 10000; ++i) {
+    if (sln.countGoodNumbers(i) != Solution_correct().countGoodNumbers(i)) {
+      std::cerr << "wrong answer, input is: " << i << "\noutput is: " << sln.countGoodNumbers(i) << "\nanswer is: " << Solution_correct().countGoodNumbers(i) << std::endl;
+      abort();
+    }
+  }
+  assert(711414395 == sln.countGoodNumbers(1000000000000000l));
+  return 0;
+}
