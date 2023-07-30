a1cde6234142046af0029edb27cead314bfa33f0
Lucius Kyle <LuciusKyle@outlook.com>
2023-07-30 09:37:48 +0000

    1922 beats 35%.

--- 1922_Count_Good_Numbers/1922.cc
+++ 1922_Count_Good_Numbers/1922.cc
@@ -9,7 +9,13 @@ class Solution {
   int countGoodNumbers(long long n) {
     if (n == 1) return 5;
     if (n == 2) return 20;
-    const long long sub_str_len = n / 2 + (n / 2 % 2 == 1 ? 1 : 0);
-    return (long long)countGoodNumbers(sub_str_len) * (long long)countGoodNumbers(n - sub_str_len) % kModulo;
+    if (answer_cache_.count(n) == 0) {
+      const long long sub_str_len = n / 2 + (n / 2 % 2 == 1 ? 1 : 0);
+      answer_cache_[n] = (long long)countGoodNumbers(sub_str_len) * (long long)countGoodNumbers(n - sub_str_len) % kModulo;
+    }
+    return answer_cache_.at(n);
   }
+
+ private:
+  std::unordered_map<long long, int> answer_cache_;
 };
