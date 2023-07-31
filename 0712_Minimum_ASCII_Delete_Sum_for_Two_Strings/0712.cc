485abdd461b745a6312f21b9b1a2bbf0f4847d53
Lucius Kyle <luciuskyle@outlook.com>
2023-07-31 18:03:03 +0000

    0712 beats 79%.

--- 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings/0712.cc
+++ 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings/0712.cc
@@ -9,29 +9,22 @@ using std::vector;
 
 class Solution {
  public:
-  int minimumDeleteSum(string s1, string s2) {
-    dp_.assign(s1.size() + 1, vector<int>(s2.size() + 1, -1));
-    return minimumDeleteSum(s1, s2, 0, 0);
-  }
-
- private:
-  vector<vector<int>> dp_;
-  int minimumDeleteSum(const string &s1, const string &s2, int i1, int i2) {
-    if (dp_[i1][i2] != -1) return dp_[i1][i2];
-    if (i1 == s1.size()) {
-      const int cost = std::accumulate(s2.cbegin() + i2, s2.cend(), int(0));
-      dp_[i1][i2] = cost;
-      return cost;
-    }
-    if (i2 == s2.size()) {
-      const int cost = std::accumulate(s1.cbegin() + i1, s1.cend(), int(0));
-      dp_[i1][i2] = cost;
-      return cost;
-    }
-    const int delete_i1_cost = int(s1[i1]) + minimumDeleteSum(s1, s2, i1 + 1, i2);
-    const int delete_i2_cost = int(s2[i2]) + minimumDeleteSum(s1, s2, i1, i2 + 1);
-    const int delete_or_keep_both_cost = (s1[i1] == s2[i2] ? 0 : int(s1[i1]) + int(s2[i2])) + minimumDeleteSum(s1, s2, i1 + 1, i2 + 1);
-    dp_[i1][i2] = std::min(delete_or_keep_both_cost, std::min(delete_i1_cost, delete_i2_cost));
-    return dp_[i1][i2];
+  int minimumDeleteSum(const string s1, const string s2) {
+    const int len1 = s1.size(), len2 = s2.size();
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+    for (int i = len2 - 1; 0 <= i; --i)
+      dp[len1][i] = dp[len1][i + 1] + int(s2[i]);
+    for (int i = len1 - 1; 0 <= i; --i)
+      dp[i][len2] = dp[i + 1][len2] + int(s1[i]);
+    for (int i1 = len1 - 1; 0 <= i1; --i1)
+      for (int i2 = len2 - 1; 0 <= i2; --i2)
+        dp[i1][i2] = std::min(std::min(int(s1[i1]) + dp[i1 + 1][i2], int(s2[i2]) + dp[i1][i2 + 1]), (s1[i1] == s2[i2] ? 0 : int(s1[i1]) + int(s2[i2])) + dp[i1 + 1][i2 + 1]);
+    return dp[0][0];
   }
 };
+
+int main(int argc, char *argv[]) {
+  Solution sln;
+  sln.minimumDeleteSum("delete", "leet");
+  return 0;
+}
