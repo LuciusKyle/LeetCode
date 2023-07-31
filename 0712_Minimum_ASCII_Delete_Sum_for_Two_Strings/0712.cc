cb2a2e64a5fd338e80f715bfa737d836b0417d09
Lucius Kyle <luciuskyle@outlook.com>
2023-07-31 18:19:01 +0000

    0712 solved.

--- 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings/0712.cc
+++ 0712_Minimum_ASCII_Delete_Sum_for_Two_Strings/0712.cc
@@ -11,15 +11,16 @@ class Solution {
  public:
   int minimumDeleteSum(const string s1, const string s2) {
     const int len1 = s1.size(), len2 = s2.size();
-    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));
+    vector<int> curr_line(len2 + 1), next_line(len2 + 1);
     for (int i = len2 - 1; 0 <= i; --i)
-      dp[len1][i] = dp[len1][i + 1] + int(s2[i]);
-    for (int i = len1 - 1; 0 <= i; --i)
-      dp[i][len2] = dp[i + 1][len2] + int(s1[i]);
-    for (int i1 = len1 - 1; 0 <= i1; --i1)
+      curr_line[i] = curr_line[i + 1] + int(s2[i]);
+    for (int i1 = len1 - 1; 0 <= i1; --i1) {
+      next_line.back() = curr_line.back() + int(s1[i1]);
       for (int i2 = len2 - 1; 0 <= i2; --i2)
-        dp[i1][i2] = std::min(std::min(int(s1[i1]) + dp[i1 + 1][i2], int(s2[i2]) + dp[i1][i2 + 1]), (s1[i1] == s2[i2] ? 0 : int(s1[i1]) + int(s2[i2])) + dp[i1 + 1][i2 + 1]);
-    return dp[0][0];
+        next_line[i2] = std::min(std::min(int(s1[i1]) + curr_line[i2], int(s2[i2]) + next_line[i2 + 1]), (s1[i1] == s2[i2] ? 0 : int(s1[i1]) + int(s2[i2])) + curr_line[i2 + 1]);
+      next_line.swap(curr_line);
+    }
+    return curr_line.front();
   }
 };
 
