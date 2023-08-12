c09afa2203a39de2202ee00db17210bf868fc49c
Lucius Kyle <LuciusKyle@outlook.com>
2023-08-12 00:10:47 +0000

    0518 solved.

--- 0518_Coin_Change_2/0518.cc
+++ 0518_Coin_Change_2/0518.cc
@@ -7,28 +7,16 @@
 using std::array;
 using std::vector;
 
-constexpr int kMaxAmount = 5000;
-
 class Solution {
  public:
   int change(const int amount, const vector<int>& coins) {
-    vector<vector<int>> answer_cache(coins.size(), vector<int>(amount + 1));
-    for (int i = 0; i < answer_cache[0].size(); i += coins[0])
-      answer_cache[0][i] = 1;
-
-    for (size_t i = 1; i < answer_cache.size(); ++i) {
-      for (int j = 0; j < amount + 1; ++j) {
-        if (answer_cache[i - 1][j]) {
-          for (int k = j; k < amount + 1; k += coins[i]) {
-            answer_cache[i][k] += answer_cache[i - 1][j];
-          }
-        }
-      }
-    }
-    return answer_cache[coins.size() - 1][amount];
+    vector<int> combinations(amount + 1);
+    combinations[0] = 1;
+    for (const int coin : coins)
+      for (int i = 0; i < amount + 1 - coin; ++i)
+        combinations[i + coin] += combinations[i];
+    return combinations.back();
   }
-
- private:
 };
 
 int main(void) {
