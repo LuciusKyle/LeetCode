3e4ed1ad7188915829ef0aeccf1178bf292e7d61
Lucius <LuciusKyle@outlook.com>
2023-08-24 08:47:17 +0000

    0767 solved.

--- 0767_Reorganize_String/0767.cc
+++ 0767_Reorganize_String/0767.cc
@@ -1,32 +1,33 @@
 
+#include <algorithm>
 #include <string>
 
 using std::string;
 
 class Solution {
  public:
-  string reorganizeString(const string S) {
-    if (S.empty() && S.size() == 1) return S;
-    int counter[26]{0};
-    for (const char ch : S) ++counter[ch - 'a'];
-    string ret;
-    size_t s_index = 0;
-    for (; s_index < 26; ++s_index)
-      if (counter[s_index]) {
-        ret.push_back(s_index + 'a');
-        --counter[s_index];
-        break;
-      }
-
-    for (++s_index; s_index < 26 * S.size(); ++s_index) {
-      if (counter[s_index % 26]) {
-        if (s_index % 26 + 'a' == ret.back()) return "";
-        ret.push_back(s_index % 26 + 'a');
-        if (ret.size() == S.size()) break;
-        --counter[s_index % 26];
-      }
+  string reorganizeString(string s) {
+    if (s.size() == 1) return s;
+    std::pair<int, char> lettrers_count[26];
+    for (char ch = 'a'; ch <= 'z'; ++ch) lettrers_count[ch - 'a'] = {0, ch};
+    for (const char ch : s) ++lettrers_count[ch - 'a'].first;
+    std::sort(std::begin(lettrers_count), std::end(lettrers_count));
+    if (lettrers_count[25].first == 1) return s;
+    if ((s.size() + 1) / 2 < lettrers_count[25].first) return "";
+    int letters_index = 25;
+    // even indexes;
+    for (int i = 0; i < s.size(); i += 2) {
+      s[i] = lettrers_count[letters_index].second;
+      --lettrers_count[letters_index].first;
+      if (lettrers_count[letters_index].first == 0) --letters_index;
     }
-    return ret;
+    // odd indexes;
+    for (int i = 1; i < s.size(); i += 2) {
+      s[i] = lettrers_count[letters_index].second;
+      --lettrers_count[letters_index].first;
+      if (lettrers_count[letters_index].first == 0) --letters_index;
+    }
+    return s;
   }
 };
 
