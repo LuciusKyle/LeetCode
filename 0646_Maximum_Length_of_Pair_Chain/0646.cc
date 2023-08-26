237eee1776bd1d42846aedc881ba401294fb10e8
Lucius Kyle <LuciusKyle@outlook.com>
2023-08-26 14:05:17 +0000

    0646 beats 67%.

--- 0646_Maximum_Length_of_Pair_Chain/0646.cc
+++ 0646_Maximum_Length_of_Pair_Chain/0646.cc
@@ -8,35 +8,21 @@ using std::vector;
 class Solution {
  public:
   int findLongestChain(vector<vector<int>>& pairs) {
-    std::sort(pairs.begin(), pairs.end(),
-              [](vector<int>& x_, vector<int>& y_) -> bool {
-                return x_.front() < y_.front();
-              });
-    vector<int> result(pairs.size(), -1);
-    const auto iter = pairs.end();
-    for (int i = 0; i < pairs.size(); ++i) {
-      
+    const int length = pairs.size();
+    std::sort(pairs.begin(), pairs.end(), [](const vector<int>& _x, const vector<int>& _y) -> bool {
+      return _x[0] == _y[0] ? _x[1] < _y[1] : _x[0] < _y[0];
+    });
+    vector<int> results(pairs.size(), 1);
+    int max_len = 1;
+    for (int len = 1; len < length; ++len) {
+      const vector<int>& curr_p = pairs[len];
+      int temp_max = 1;
+      for (int i = 0; i < len; ++i)
+        if (pairs[i][1] < curr_p[0])
+          temp_max = std::max(temp_max, results[i] + 1);
+      results[len] = temp_max;
+      max_len = std::max(max_len, temp_max);
     }
-    return 1;
-  }
-
- private:
-  int findLongestChain(const vector<vector<int>>& pairs, const int cur_index,
-                       vector<int>& result) {
-    return 0;
+    return max_len;
   }
 };
-
-int main(void) {
-  Solution sln;
-
-  const vector<int> test{0, 10, 20, 30, 40, 50, 60, 70, 80, 90};
-  auto iter = std::upper_bound(test.begin(), test.end(), 90);
-  if (iter == test.end())
-    std::cout << "reach end" << std::endl;
-  else
-    std::cout << test.end() - iter << std::endl;
-
-  return 0;
-}
-
