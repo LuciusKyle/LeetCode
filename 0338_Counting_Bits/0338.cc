1fe631a3510802882b869a4b07d9954757fd9b4f
Lucius Kyle <LuciusKyle@outlook.com>
2023-09-01 04:31:10 +0000

    0338 solved.

--- 0338_Counting_Bits/0338.cc
+++ 0338_Counting_Bits/0338.cc
@@ -8,23 +8,34 @@ using std::vector;
 class Solution {
  public:
   vector<int> countBits(int n) {
-    vector<int> result;
-    result.reserve(n + 1);
-    for (int i = 0; i <= n; ++i) {
-      int count_ones = 0;
-      int num = i;
-      while (num != 0) {
-        if (num & 1) ++count_ones;
-        num = num >> 1;
+    vector<int> result(n + 1);
+    for (int i = 1, restart_num = 1; i < n + 1; ++i)
+      if (i == restart_num) {
+        result[i] = 1;
+        restart_num *= 2;
+      } else {
+        result[i] = result[i - restart_num / 2] + 1;
       }
-      result.push_back(count_ones);
-    }
     return result;
   }
 };
 
+// official solution
+class Solution_official {
+ public:
+  vector<int> countBits(int n) {
+    vector<int> counts(n + 1);
+    // For any number x > 0, if we remove the least significant bit of x, then its bit count will reduce by 1.
+    for (int i = 1; i <= n; ++i) {
+      uint least_significant_bit = ~(i - 1) & i;
+      counts[i] = 1 + counts[i ^ least_significant_bit];
+    }
+    return counts;
+  }
+};
+
 int main(int argc, char *argv[]) {
-  for (int i = 0; i <= 100000; ++i) {
+  for (int i = 0; i <= 1026; ++i) {
     int count_ones = 0;
     int num = i;
     while (num != 0) {
