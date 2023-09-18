d5f178f39da9653f008a3af8ad11250541225585
Lucius <LuciusKyle@outlook.com>
2023-09-18 06:29:02 +0000

    1337 solved.

--- 1337_The_K_Weakest_Rows_in_a_Matrix/1337.cc
+++ 1337_The_K_Weakest_Rows_in_a_Matrix/1337.cc
@@ -7,13 +7,23 @@ using std::vector;
 class Solution {
  public:
   vector<int> kWeakestRows(const vector<vector<int>>& mat, int k) {
-    std::pair<int, int> count_index;
-    std::priority_queue<decltype(count_index) /*, std::vector<decltype(count_index)>, decltype([](const decltype(count_index) &x_, const decltype(count_index) &y_) -> bool { return x_.first > y_.first ? true : (x_.second > y_.second); })*/> pq;
+    const int row_size = mat.size(), column_size = mat[0].size();
+    std::priority_queue<std::pair<int, int>> pq;
     for (int row = 0; row < mat.size(); ++row) {
-      count_index = {0, row};
-      for (const int num : mat[row])
-        count_index.first += num;
-      pq.push(count_index);
+      int l_index = 0, r_index = column_size - 1;
+      while (l_index + 1 < r_index) {
+        const int mid_index = (l_index + r_index) / 2;
+        if (mat[row][mid_index] == 1)
+          l_index = mid_index;
+        else
+          r_index = mid_index;
+      }
+      int addition = 0;
+      if (mat[row][l_index] == 1 && mat[row][r_index] == 0)
+        addition = 1;
+      else if (mat[row][l_index] == 1 && mat[row][r_index] == 1)
+        addition = 2;
+      pq.emplace(l_index + addition, row);
       if (k < pq.size()) pq.pop();
     }
     vector<int> answer(k);
@@ -24,3 +34,10 @@ class Solution {
     return answer;
   }
 };
+
+int main(int argc, char* argv[]) {
+  Solution sln;
+  sln.kWeakestRows({{1, 1, 0, 0, 0}, {1, 1, 1, 1, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 0, 0}, {1, 1, 1, 1, 1}}, 3);
+  sln.kWeakestRows({{1, 0, 0, 0}, {1, 1, 1, 1}, {1, 0, 0, 0}, {1, 0, 0, 0}}, 2);
+  return 0;
+}
