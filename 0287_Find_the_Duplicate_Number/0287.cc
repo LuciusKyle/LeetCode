db187b5c2a5245202f1355325809593ed53a5707
Lucius Kyle <LuciusKyle@outlook.com>
2023-09-19 01:52:56 +0000

    0287 beats 79%.

--- 0287_Find_the_Duplicate_Number/0287.cc
+++ 0287_Find_the_Duplicate_Number/0287.cc
@@ -1,25 +1,35 @@
 
 #include <assert.h>
+#include <math.h>
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
 #include <vector>
 
+using std::cout;
+using std::endl;
 using std::vector;
 
 class Solution {
  public:
-  int findDuplicate(const vector<int>& nums) {
-    vector<int> dict(nums.size(), -1);
-    for (const int num : nums)
-      if (dict[num] != -1)
-        return num;
+  int findDuplicate(vector<int>& nums) {
+    for (const int num : nums) {
+      if (nums[std::abs(num)] < 0)
+        return std::abs(num);
       else
-        dict[num] = 0;
-
+        nums[std::abs(num)] = -nums[std::abs(num)];
+    }
     return -1;
   }
 };
 
 int main(void) {
   Solution sln;
-  assert(2 == sln.findDuplicate({1, 3, 4, 2, 2}));
+  // assert(2 == sln.findDuplicate({1, 3, 4, 2, 2}));
+  for (int i = 1, sum = 0; i < 10; ++i) {
+    sum += i;
+    cout << "i: " << i << "\tsum: " << sum << "\n";
+  }
   return 0;
 }
